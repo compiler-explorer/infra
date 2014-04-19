23dab42ad5e1d96dc911a0279dfd61fda29b3631
Matt Godbolt <mgodbolt@drwholdings.com>
2014-04-18 21:24:59 -0500

    More work towards a first go

--- setup.sh
+++ setup.sh
@@ -23,3 +23,44 @@ su -c "make node_modules" gcc-user
 
 mkdir /var/cache/nginx-gcc
 chown www-data /var/cache/nginx-gcc
+
+cp nginx-conf /etc/sites-available/default
+service nginx reload
+
+mkdir /opt
+cd /opt
+cat > ~/.s3cfg <<EOF
+[default]
+access_key = ${S3_ACCESS_KEY}
+secret_key = ${S3_SECRET_KEY}
+EOF
+
+for f in clang-3.2.tar.gz \
+    clang-3.3.tar.gz \
+    gcc-4.9.0-0909-concepts.tar.gz \
+    gcc-4.9.0-with-concepts.tar.gz \
+    ; do
+s3cmd get s3://gcc-explorer/opt/$f
+tar zxf $f
+rm $f
+done
+
+# Intel compiler
+for f in 
+s3cmd get s3://gcc-explorer/opt/l_ccompxe_2013.1.117.tar.xz
+tar Jxf l_ccompxe_2013.1.117.tar.xz
+cat > /tmp/install.sh <<EOF
+SEND_USAGE_DATA=no
+PSET_SERIAL_NUMBER=${INTEL_SERIAL_NUMBER}
+ACTIVATION=serial_number
+CONTINUE_WITH_INSTALLDIR_OVERWRITE=yes
+CONTINUE_WITH_OPTIONAL_ERROR=yes
+PSET_INSTALL_DIR=/opt/intel/composer_xe_2013.1.117
+INSTALL_MODE=NONRPM
+ACCEPT_EULA=accept
+EOF
+cd l_ccompxe_2013.1.117
+./install.sh --silent /tmp/install.sh
+cd ..
+rm -rf l_ccompxe_2013.1.117.tar.xz l_ccompxe_2013.1.117
+
