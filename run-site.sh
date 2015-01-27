4ff3ad28ed79f1502c4297077bc67e7bcd30f373
Matt Godbolt <mgodbolt@drw.com>
2015-01-27 14:21:02 -0600

    Yay, working container

--- run-site.sh
+++ run-site.sh
@@ -2,8 +2,13 @@
 
 set -ex
 
-GCC=$(sudo docker run -d -p 10240:10240 gcc-explorer)
-D=$(sudo docker run -d -p 10241:10241 d-explorer)
-RUST=$(sudo docker run -d -p 10242:10242 rust-explorer)
+GCC=$(sudo docker run --name gcc -d -p 10240:10240 gcc-explorer)
+D=$(sudo docker run --name d -d -p 10241:10241 d-explorer)
+RUST=$(sudo docker run --name rust -d -p 10242:10242 rust-explorer)
 
-sudo docker run -p 80:80 -v /var/log/nginx:/var/log/nginx -v $(pwd)/nginx:/etc/nginx/sites-enabled dockerfile/nginx
+sudo docker run \
+	-p 80:80 \
+	-v /var/log/nginx:/var/log/nginx \
+	-v $(pwd)/nginx:/etc/nginx/sites-enabled \
+	--link gcc:gcc --link d:d --link rust:rust \
+	dockerfile/nginx
