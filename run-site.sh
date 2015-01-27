5303fa176d7640ed534218e7b03bd88604904eb2
Matt Godbolt <mgodbolt@drw.com>
2015-01-27 14:23:35 -0600

    More fix

--- run-site.sh
+++ run-site.sh
@@ -2,9 +2,13 @@
 
 set -ex
 
+sudo docker rm gcc d rust || true
+
 GCC=$(sudo docker run --name gcc -d -p 10240:10240 gcc-explorer)
 D=$(sudo docker run --name d -d -p 10241:10241 d-explorer)
 RUST=$(sudo docker run --name rust -d -p 10242:10242 rust-explorer)
+
+trap "sudo docker stop $GCC $D $RUST" SIGINT SIGTERM
 
 sudo docker run \
 	-p 80:80 \
