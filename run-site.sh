1398c8d4adb04963b3dbd390fad7b3d65add067b
Matt Godbolt <matt@godbolt.org>
2015-01-30 08:10:01 -0600

    Container monkery

--- run-site.sh
+++ run-site.sh
@@ -8,11 +8,12 @@ if [[ $UID = 0 ]]; then
 fi
 
 # TODO:
-# check caching works in nginx
-# check STH proxy works
+# check caching works in nginx (seemingly yes for gcc explorer)
+# check STH proxy works - it does, but seemingly doesn't cache?
+# access.log seems not reliable for bbc
 
-$SUDO docker stop gcc d rust || true
-$SUDO docker rm gcc d rust || true
+$SUDO docker stop gcc d rust nginx || true
+$SUDO docker rm gcc d rust nginx || true
 
 $SUDO docker pull mattgodbolt/gcc-explorer
 
@@ -20,10 +21,11 @@ GCC=$(sudo docker run --name gcc -d -p 10240:10240 mattgodbolt/gcc-explorer:gcc)
 D=$(sudo docker run --name d -d -p 10241:10241 mattgodbolt/gcc-explorer:d)
 RUST=$(sudo docker run --name rust -d -p 10242:10242 mattgodbolt/gcc-explorer:rust)
 
-trap "$SUDO docker stop $GCC $D $RUST" SIGINT SIGTERM
+trap "$SUDO docker stop $GCC $D $RUST" SIGINT SIGTERM SIGPIPE
 
 $SUDO docker run \
     -p 80:80 \
+    --name nginx \
     --volumes-from gcc \
     -v /var/log/nginx:/var/log/nginx \
     -v /home/ubuntu:/var/www \
