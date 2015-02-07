d5090dff60b748d9da3be493609be3864bdc3d22
Matt Godbolt <matt@godbolt.org>
2015-02-07 16:54:07 -0600

    Oopsie

--- user-data-script.sh
+++ user-data-script.sh
@@ -3,15 +3,15 @@
 set -ex
 
 mkdir -p ~/.ssh
-echo > ~/.ssh/known_hosts <<EOF
+cat > ~/.ssh/known_hosts <<EOF
 |1|6kkg4vXFndu74Cif5Oz9098PsY4=|k0sy2nylJdzYTwQK+aKmITvX8S8= ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==
 |1|9zKzfbCf91lj6RBfxx8gmxHV7Lc=|9JCgnqPKdeIaFvFF0tijjEgBWOk= ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==
 EOF
-echo > ~/.ssh/id_rsa <<EOF
+cat > ~/.ssh/id_rsa <<EOF
 ${PRIVATE_KEY}
 EOF
 chmod 600 ~/.ssh/id_rsa
-echo > ~/.ssh/id_rsa.pub <<EOF
+cat > ~/.ssh/id_rsa.pub <<EOF
 ${PUBLIC_KEY}
 EOF
 echo -n '${DOCKER_CFG}' > ~/.dockercfg
