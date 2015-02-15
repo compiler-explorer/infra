4f2eb78eb9ba2ea9141a54e652bd5bcbb2a3d2b0
Matt Godbolt <matt@godbolt.org>
2015-02-15 14:16:34 -0600

    Oops

--- docker/gcc-explorer/run.sh
+++ docker/gcc-explorer/run.sh
@@ -4,7 +4,7 @@ set -ex
 
 . /site.sh
 
-[[ ! -e /gcc-explorer] && git clone -b ${BRANCH} --depth 1 https://github.com/mattgodbolt/gcc-explorer.git /gcc-explorer
+[[ ! -e /gcc-explorer/.git ]] && git clone -b ${BRANCH} --depth 1 https://github.com/mattgodbolt/gcc-explorer.git /gcc-explorer
 cd /gcc-explorer
 git pull
 make prereqs
