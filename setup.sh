3f0b4fc72bd14c53c3437113343f12e047ad3920
Matt Godbolt <matt@godbolt.org>
2015-02-25 22:35:03 -0600

    moose

--- setup.sh
+++ setup.sh
@@ -37,7 +37,7 @@ get_or_update_repo() {
     popd
 }
 
-PTRAIL='/etc/rsyslog.d/99-papertrail.conf '
+PTRAIL='/etc/rsyslog.d/99-papertrail.conf'
 if [[ ! -f "${PTRAIL}" ]]; then
     echo '*.*          @logs2.papertrailapp.com:34474' > "${PTRAIL}"
     service rsyslog restart
@@ -54,10 +54,11 @@ destination:
   protocol: tls
 EOF
     docker pull gliderlabs/logspout:latest
+    popd
 fi
 
 docker rm logspout || true
-docker run --name logspout -d -v=/var/run/docker.sock:/tmp/docker.sock gliberlabs/logspout syslog://logs2.papertrailapp.com:34474
+docker run --name logspout -d -v=/var/run/docker.sock:/tmp/docker.sock gliderlabs/logspout syslog://logs2.papertrailapp.com:34474
 
 apt-get -y update
 apt-get -y upgrade --force-yes
