47107a0b2fd41e1335757adf047cdf4b576cf29e
Matt Godbolt <matt@godbolt.org>
2015-04-22 15:23:28 -0500

    GCC 5.1

--- docker/gcc-explorer/compilers.sh
+++ docker/gcc-explorer/compilers.sh
@@ -6,3 +6,11 @@ cd /opt
 curl http://llvm.org/releases/3.5.1/clang+llvm-3.5.1-x86_64-linux-gnu.tar.xz | tar Jxf -
 
 find /opt -executable -type f | xargs strip || true
+
+# gcc 5.1 doesn't like being stripped
+for compiler in gcc-5.1.0.tar.gz \
+    ; do
+    s3cmd --config /root/.s3cfg get s3://gcc-explorer/opt/$compiler /opt/$compiler
+    tar zxf $compiler
+    rm $compiler
+done
