a3503fa00a4c1f30932bbd539ed828b0f4bc9d4e
Matt Godbolt <matt@godbolt.org>
2015-04-30 07:33:34 -0500

    Don't install DMD

--- docker/d-explorer/compilers.sh
+++ docker/d-explorer/compilers.sh
@@ -10,9 +10,3 @@ rm i686-linux-gnu_2.066.1_gcc4.9.2_f378f9ab41_20150405.tar.xz
 wget http://gdcproject.org/downloads/binaries/x86_64-linux-gnu/x86_64-linux-gnu_2.066.1_gcc4.9.2_f378f9ab41_20150405.tar.xz
 tar Jxf x86_64-linux-gnu_2.066.1_gcc4.9.2_f378f9ab41_20150405.tar.xz
 rm x86_64-linux-gnu_2.066.1_gcc4.9.2_f378f9ab41_20150405.tar.xz
-
-DMD_VERSION="2.066.1-0"
-rm -f ${DMD_VERSION}_amd64.deb
-wget http://downloads.dlang.org/releases/2014/dmd_${DMD_VERSION}_amd64.deb
-dpkg -i dmd_${DMD_VERSION}_amd64.deb
-rm dmd_${DMD_VERSION}_amd64.deb
