3524f378965be58533b74736f8e94913e6a4f456
Matt Godbolt <matt@godbolt.org>
2015-06-25 13:57:56 -0500

    Rust

--- docker/rust-explorer/compilers.sh
+++ docker/rust-explorer/compilers.sh
@@ -3,25 +3,31 @@
 set -e
 
 cd /opt
-curl http://static.rust-lang.org/dist/rust-nightly-x86_64-unknown-linux-gnu.tar.gz | tar zxf -
-cd rust-nightly-x86_64-unknown-linux-gnu
-./install.sh --prefix=/opt/rust-nightly
-cd /opt
-rm -rf rust-nightly-x86_64-unknown-linux-gnu
-
-# workaround for LD_LIBRARY_PATH
-cd /opt
 curl http://nixos.org/releases/patchelf/patchelf-0.8/patchelf-0.8.tar.gz | tar zxf -
 cd patchelf-0.8
 ./configure
 make
-for to_patch in /opt/rust-nightly/bin/rustc $(find /opt/rust-nightly/lib -name *.so); do
-	src/patchelf --set-rpath /opt/rust-nightly/lib $to_patch
-done
-cd ..
-rm -rf patchelf-0.8
 
-# Don't need docs
-rm -rf /opt/rust-nightly/share
+install_rust() {
+    local NAME=$1
 
+    cd /opt
+    curl http://static.rust-lang.org/dist/rust-${NAME}-x86_64-unknown-linux-gnu.tar.gz | tar zxf -
+    cd rust-${NAME}-x86_64-unknown-linux-gnu
+    ./install.sh --prefix=/opt/rust-${NAME}
+    cd /opt
+    rm -rf rust-${NAME}-x86_64-unknown-linux-gnu
+    
+    # workaround for LD_LIBRARY_PATH
+    for to_patch in /opt/rust-${NAME}/bin/rustc $(find /opt/rust-${NAME}/lib -name *.so); do
+    	/opt/patchelf-0.8/src/patchelf --set-rpath /opt/rust-${NAME}/lib $to_patch
+    done
+    
+    # Don't need docs
+    rm -rf /opt/rust-${NAME}/share
+}
+
+install_rust nightly
+install_rust 1.0.0
 find /opt -executable -type f | xargs strip || true
+rm -rf /opt/patchelf-0.8
