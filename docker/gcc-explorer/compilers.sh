86da2adca07b7cec7072112bd506576bab316b64
Matt Godbolt <matt@godbolt.org>
2016-01-13 21:28:10 -0600

    llvm3.7.1

--- docker/gcc-explorer/compilers.sh
+++ docker/gcc-explorer/compilers.sh
@@ -7,6 +7,7 @@ curl http://llvm.org/releases/3.5.1/clang+llvm-3.5.1-x86_64-linux-gnu.tar.xz | t
 curl http://llvm.org/releases/3.5.2/clang+llvm-3.5.2-x86_64-linux-gnu-ubuntu-14.04.tar.xz | tar Jxf -
 curl http://llvm.org/releases/3.6.2/clang+llvm-3.6.2-x86_64-linux-gnu-ubuntu-14.04.tar.xz | tar Jxf -
 curl http://llvm.org/releases/3.7.0/clang+llvm-3.7.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz | tar Jxf -
+curl http://llvm.org/releases/3.7.1/clang+llvm-3.7.1-x86_64-linux-gnu-ubuntu-14.04.tar.xz | tar Jxf -
 
 find /opt -executable -type f | xargs strip || true
 
