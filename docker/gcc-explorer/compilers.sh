f35044167e1e87e26da197ba51602b505951ac81
Matt Godbolt <matt@godbolt.org>
2016-03-24 23:23:08 -0500

    Clang 3.8.0

--- docker/gcc-explorer/compilers.sh
+++ docker/gcc-explorer/compilers.sh
@@ -8,6 +8,7 @@ curl http://llvm.org/releases/3.5.2/clang+llvm-3.5.2-x86_64-linux-gnu-ubuntu-14.
 curl http://llvm.org/releases/3.6.2/clang+llvm-3.6.2-x86_64-linux-gnu-ubuntu-14.04.tar.xz | tar Jxf -
 curl http://llvm.org/releases/3.7.0/clang+llvm-3.7.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz | tar Jxf -
 curl http://llvm.org/releases/3.7.1/clang+llvm-3.7.1-x86_64-linux-gnu-ubuntu-14.04.tar.xz | tar Jxf -
+curl http://llvm.org/releases/3.8.0/clang+llvm-3.8.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz | tar Jxf -
 
 find /opt -executable -type f | xargs strip || true
 
