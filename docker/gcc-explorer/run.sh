1acd1e013097db3bebf592958d9ef2a76b084342
Matt Godbolt <matt@godbolt.org>
2016-05-30 09:32:21 -0500

    Minor fixups

--- docker/gcc-explorer/run.sh
+++ docker/gcc-explorer/run.sh
@@ -10,6 +10,6 @@ git pull
 rm -rf node_modules
 cp -r /tmp/node_modules .
 make prereqs
-forever start -a -f -d -v -c \
+./node_modules/.bin/forever -a -f -d -v -c \
 	"node --prof --track_gc_object_stats --trace_gc_verbose --optimize_for_size --log_timer_events" \
 	app.js --env amazon --port 10240 --lang C++
