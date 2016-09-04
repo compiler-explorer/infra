c919c6eaa927514935df332e5c2842f53e2eaabd
Matt Godbolt <matt@godbolt.org>
2016-09-04 11:49:29 -0500

    More fixes, support spot instances

--- build_gcc.py
+++ build_gcc.py
@@ -4,6 +4,7 @@ import boto3
 import time
 import paramiko
 import select
+import datetime
 import sys
 from argparse import ArgumentParser
 
@@ -14,6 +15,11 @@ parser.add_argument('--not-ebs', help='Do not use an EBS optimized instance', ac
         default=False)
 parser.add_argument('version', help='Build GCC version')
 parser.add_argument('destination', help='Build destination s3 URL')
+parser.add_argument('--spot-price', help='Set hourly spot price')
+
+
+def log(msg):
+    print '{} : {}'.format(datetime.datetime.now(), msg)
 
 
 def connect_ssh(addr, username='rancher'):
@@ -21,12 +27,12 @@ def connect_ssh(addr, username='rancher'):
     ssh.load_system_host_keys()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     while True:
-        print "Connecting to {}...".format(addr)
+        log("Connecting to {}...".format(addr))
         try:
             ssh.connect(addr, username=username)
         except Exception, e:
-            print "Got exception {}".format(e)
-            print "Retrying..."
+            log("Got exception {}".format(e))
+            log("Retrying...")
             time.sleep(10)
             continue
         break
@@ -41,7 +47,7 @@ def run_command(ssh, command):
     while True:
         if channel.exit_status_ready():
             status = channel.recv_exit_status()
-            print "Command returned exit code {}".format(status)
+            log("Command returned exit code {}".format(status))
             return status
         rl, wl, xl = select.select([channel], [], [], 5)
         if rl:
@@ -51,37 +57,77 @@ def run_command(ssh, command):
 if __name__ == '__main__':
     args = parser.parse_args()
     ec2 = boto3.resource('ec2')
-    print "Launching a {} instance...".format(args.instance_type)
-    instances = ec2.create_instances(
-            ImageId='ami-1071ca07',
-            KeyName='mattgodbolt',
-            SecurityGroupIds=['sg-cdce6cb7'],
-            SubnetId='subnet-690ed81e',
-            InstanceType=args.instance_type,
-            EbsOptimized=not args.not_ebs,
-            BlockDeviceMappings=[
-                {
-                    'DeviceName':'/dev/sda1', 
-                    'Ebs': {'VolumeSize': 32, 'DeleteOnTermination': True}
-                }
-            ],
-            MinCount=1,
-            MaxCount=1,
-            IamInstanceProfile={'Name':'GccBuilder'})
-    if len(instances) != 1:
-        raise "Wrong number of instances"
-    instance = instances[0]
-    print "Waiting for instance {} to become running".format(instance)
+    client = boto3.client('ec2')
+    if args.spot_price:
+        log("Requesting a {} spot instance at ${}...".format(args.instance_type, args.spot_price))
+        response = client.request_spot_instances(
+                SpotPrice=args.spot_price,
+                Type='one-time',
+                BlockDurationMinutes=60,
+                LaunchSpecification={
+                    'ImageId': 'ami-1071ca07',
+                    'KeyName': 'mattgodbolt',
+                    'SecurityGroupIds': ['sg-cdce6cb7'],
+                    'SubnetId': 'subnet-690ed81e',
+                    'InstanceType': args.instance_type,
+                    'EbsOptimized': not args.not_ebs,
+                    'BlockDeviceMappings': [
+                        {
+                            'DeviceName':'/dev/sda1', 
+                            'Ebs': {'VolumeSize': 32, 'DeleteOnTermination': True}
+                            }
+                        ],
+                    'IamInstanceProfile': {'Name':'GccBuilder'}
+                    })
+        sir = response['SpotInstanceRequests']
+        if len(sir) != 1:
+            raise "Wrong number of instances"
+        si = sir[0]
+        waiter = client.get_waiter('spot_instance_request_fulfilled')
+        spot_ids=[si['SpotInstanceRequestId']]
+        waiter.wait(SpotInstanceRequestIds=spot_ids)
+        sir = client.describe_spot_instance_requests(SpotInstanceRequestIds=spot_ids)
+        si = sir['SpotInstanceRequests'][0]
+        if not 'InstanceId' in si:
+            log("No instance spawned, status = {}, canceling".format(si['Status']))
+            client.cancel_spot_instance_requests(SpotInstanceRequestIds=spot_ids)
+            sys.exit(1)
+
+        log("Got instance {} at a price of {}".format(si['InstanceId'], si['ActualBlockHourlyPrice']))
+        instance = ec2.Instance(id=si['InstanceId'])
+        instance.load()
+    else:
+        log("Launching a {} instance...".format(args.instance_type))
+        instances = ec2.create_instances(
+                ImageId='ami-1071ca07',
+                KeyName='mattgodbolt',
+                SecurityGroupIds=['sg-cdce6cb7'],
+                SubnetId='subnet-690ed81e',
+                InstanceType=args.instance_type,
+                EbsOptimized=not args.not_ebs,
+                BlockDeviceMappings=[
+                    {
+                        'DeviceName':'/dev/sda1', 
+                        'Ebs': {'VolumeSize': 32, 'DeleteOnTermination': True}
+                    }
+                ],
+                MinCount=1,
+                MaxCount=1,
+                IamInstanceProfile={'Name':'GccBuilder'})
+        if len(instances) != 1:
+            raise "Wrong number of instances"
+        instance = instances[0]
+    log("Waiting for instance {} to become running".format(instance))
     time.sleep(30)
     while True:
         instance.load()
         state = instance.state['Name']
-        print "State = {}".format(state)
+        log("State = {}".format(state))
         if state == "running":
             break
         time.sleep(10)
     addr = instance.public_ip_address
-    print "Got address {}".format(addr)
+    log("Got address {}".format(addr))
 
     ec2.create_tags(
             Resources=[instance.id],
@@ -89,7 +135,7 @@ if __name__ == '__main__':
 
     ssh = connect_ssh(addr)
 
-    print "Resizing disc"
+    log("Resizing disc")
     run_command(ssh, """
 docker run --privileged -i --rm ubuntu bash << EOF
 apt-get update
@@ -100,19 +146,19 @@ resize2fs /dev/xvda1
 EOF
 """)
 
-    print "Building GCC {} to {}".format(args.version, args.destination)
+    log("Building GCC {} to {}".format(args.version, args.destination))
     res = run_command(ssh, "docker run mattgodbolt/gcc-builder bash build.sh {} {}".format(
         args.version, args.destination))
     
-    print "Shutting down instance"
+    log("Shutting down instance")
     run_command(ssh, "sudo shutdown -h now")
 
     time.sleep(10)
 
     if res == 0:
-        print "Terminating instance"
+        log("Terminating instance")
         instance.terminate()
     else:
-        print "NOT terminating instance as build failed. Stopping instead"
+        log("NOT terminating instance as build failed. Stopping instead")
         instance.stop()
 
