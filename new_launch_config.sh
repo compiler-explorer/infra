b2ccadc5212a2a6d44511b5bbd991644bc5b5258
Matt Godbolt <matt@godbolt.org>
2016-11-15 06:12:24 -0600

    Needs more space now

--- new_launch_config.sh
+++ new_launch_config.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws autoscaling create-launch-configuration --launch-configuration-name $1 --image-id $2 --instance-type t2.medium --associate-public-ip-address --iam-instance-profile XaniaBlog --security-groups sg-99df30fd --key-name mattgodbolt --block-device-mappings '[{ "DeviceName": "/dev/sda1", "Ebs": {"VolumeSize": 9, "VolumeType":"gp2", "DeleteOnTermination": true}}]' --instance-monitoring Enabled=False
+aws autoscaling create-launch-configuration --launch-configuration-name $1 --image-id $2 --instance-type t2.medium --associate-public-ip-address --iam-instance-profile XaniaBlog --security-groups sg-99df30fd --key-name mattgodbolt --block-device-mappings '[{ "DeviceName": "/dev/sda1", "Ebs": {"VolumeSize": 12, "VolumeType":"gp2", "DeleteOnTermination": true}}]' --instance-monitoring Enabled=False
