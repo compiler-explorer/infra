63321b408aa20ccac38f04694e72ea64e9704855
Matt Godbolt <mgodbolt@drw.com>
2016-12-21 18:50:14 -0600

    More rust fixes

--- docker/rust-explorer/run.sh
+++ docker/rust-explorer/run.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
 set -ex
-
 . /site.sh
 
-export LIBRARY_PATH=/usr/glibc-compat/lib
-export LD_LIBRARY_PATH=/usr/glibc-compat/lib
+/update.sh
 
-git clone -b ${BRANCH} --depth 1 https://github.com/mattgodbolt/gcc-explorer.git /gcc-explorer
-cd /gcc-explorer
-cp -r /tmp/node_modules .
-make dist
 node app.js --env amazon --language rust --port 10242 --static out/dist --archivedVersions /opt/gcc-explorer-archive
