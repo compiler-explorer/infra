91090d267b36a228f57a9da74682100c42a61b4d
Matt Godbolt <mgodbolt@drw.com>
2017-02-18 10:29:28 -0600

    Support extra args (for beta)

--- docker/go-explorer/run.sh
+++ docker/go-explorer/run.sh
@@ -4,4 +4,4 @@ set -ex
 
 . /site.sh
 
-node app.js --env amazon --language go --port 10243 --static out/dist --archivedVersions /opt/compiler-explorer-archive
+node app.js --env amazon --language go --port 10243 --static out/dist --archivedVersions /opt/compiler-explorer-archive ${EXTRA_ARGS}
--- site-beta.sh
+++ site-beta.sh
@@ -1,3 +1,4 @@
 # Sourced when running in beta mode
 export BRANCH=beta
 export EXTERNAL_PORT=80
+export EXTRA_ARGS='--env beta'
