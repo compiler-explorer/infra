ebcceabd041b26d0ef7bb66d917da908f1c531ca
Matt Godbolt <matt@godbolt.org>
2017-03-04 09:33:02 -0600

    Clang 4.0.0 homebuild

--- update_compilers/install_compilers.sh
+++ update_compilers/install_compilers.sh
@@ -325,6 +325,7 @@ done
 # Custom-built clangs also stripped and UPX'd
 for version in \
     3.9.1 \
+    4.0.0 \
 ; do
     if [[ ! -d clang-${version} ]]; then
         compiler=clang-${version}.tar.xz
