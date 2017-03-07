af56e3086455f8b7fe261c101c971a70835ab58e
Matt Godbolt <mgodbolt@drw.com>
2017-03-07 07:29:23 -0600

    Language capitalision (see mattgodbolt/compiler-explorer#303)

--- docker/go-explorer/run.sh
+++ docker/go-explorer/run.sh
@@ -4,4 +4,4 @@ set -ex
 
 . /site.sh
 
-node app.js ${EXTRA_ARGS} --env amazon --language go --port 10243 --static out/dist --archivedVersions /opt/compiler-explorer-archive
+node app.js ${EXTRA_ARGS} --env amazon --language Go --port 10243 --static out/dist --archivedVersions /opt/compiler-explorer-archive
