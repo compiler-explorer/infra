11269f0c222e59ca4a7a118a7cf140f652b02407
Matt Godbolt <matt@godbolt.org>
2017-04-08 07:48:26 -0500

    Remove old compilers from s3

---
+++ remove_old_compilers.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+KEEP_LAST=5
+S3=s3://compiler-explorer/opt/
+
+remove_older() {
+    local compiler=$1
+    for old_file in $(aws s3 ls ${S3} | grep -oE "${compiler}"'-trunk-[0-9]+.*' | sort | head -n -${KEEP_LAST}); do
+        echo Removing ${S3}${old_file}
+        aws s3 rm ${S3}${old_file}
+    done
+}
+
+remove_older clang
+remove_older gcc
