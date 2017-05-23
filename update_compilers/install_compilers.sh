534837281ffc02e8d92791f7598b9bf3d80401d0
Matt Godbolt <matt@godbolt.org>
2017-05-22 21:11:12 -0500

    Re-enable rust-nightly. Thanks to https://github.com/rust-lang/rust/pull/42124

--- update_compilers/install_compilers.sh
+++ update_compilers/install_compilers.sh
@@ -97,8 +97,7 @@ install_new_rust() {
 
 #########################
 # RUST
-# temporarily disabled as it tries to install completions in /etc
-#install_new_rust nightly '1 day'
+install_new_rust nightly '1 day'
 install_new_rust beta '1 week'
 install_new_rust 1.5.0
 install_new_rust 1.6.0
