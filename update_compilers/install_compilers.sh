73e084509505199665bf3fa48dd54ac7d25a0379
Matt Godbolt <mgodbolt@drw.com>
2017-06-21 22:01:01 -0500

    Fix up permissions for swift

--- update_compilers/install_compilers.sh
+++ update_compilers/install_compilers.sh
@@ -457,6 +457,8 @@ get_swift() {
         mkdir ${DIR}
         pushd ${DIR}
         fetch https://swift.org/builds/swift-${VER}-release/ubuntu1604/swift-${VER}-RELEASE/swift-${VER}-RELEASE-ubuntu16.04.tar.gz | tar zxf - --strip-components 1
+        # work around insane installation issue
+        chmod og+r ${DIR}/usr/lib/swift/CoreFoundation/*
         popd
     fi
 }
