7140096a0e2b0371155b169b8975b9233980ce8c
Matt Godbolt <mgodbolt@drw.com>
2017-09-19 09:25:21 -0500

    Add opencv (CC @adishavit)

--- update_compilers/install_compilers.sh
+++ update_compilers/install_compilers.sh
@@ -505,6 +505,7 @@ get_or_sync() {
 get_or_sync libs/cmcstl2 https://github.com/CaseyCarter/cmcstl2.git 
 get_or_sync libs/GSL https://github.com/Microsoft/GSL.git
 get_or_sync libs/gsl-lite https://github.com/martinmoene/gsl-lite.git
+get_or_sync libs/opencv https://github.com/opencv/opencv.git
 
 #########################
 # node.js
