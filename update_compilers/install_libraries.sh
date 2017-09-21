f106fa9abc1d54904a2ee899fc05fb0de4520bef
Rubén <rubrinbla@gmail.com>
2017-09-21 15:01:16 +0200

    Separate lib installation to its own file
    This makes it easier to test new libs

---
+++ update_compilers/install_libraries.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+# This script installs all the libraries to be used by
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+. ${DIR}/common.inc
+
+#########################
+# C++
+
+# add kvasir libraries
+if [ ! -d "libs/kvasir/mpl/trunk" ]; then
+    git clone https://github.com/kvasir-io/mpl.git libs/kvasir/mpl/trunk
+    git -C libs/kvasir/mpl/trunk checkout development
+else
+    git -C libs/kvasir/mpl/trunk pull origin development
+fi
+
+install_boost() {
+    local VERSION=$1
+    local VERSION_UNDERSCORE=$(echo ${VERSION} | tr . _)
+    local DEST=${OPT}/libs/boost_${VERSION_UNDERSCORE}/boost/ 
+    if [[ ! -d ${DEST} ]]; then
+        mkdir -p /tmp/boost
+        pushd /tmp/boost
+        fetch https://dl.bintray.com/boostorg/release/${VERSION}/source/boost_${VERSION_UNDERSCORE}.tar.bz2 | tar jxf - boost_${VERSION_UNDERSCORE}/boost
+        mkdir -p ${OPT}/libs/boost_${VERSION_UNDERSCORE}/boost
+        rsync -a boost_${VERSION_UNDERSCORE}/boost/ ${DEST}
+        popd
+        rm -rf /tmp/boost
+    fi
+}
+install_boost 1.64.0
+install_boost 1.65.0
+
+get_or_sync() {
+    local DIR=$1
+    local URL=$2
+    if [ ! -d "${DIR}" ]; then
+        git clone "${URL}" "${DIR}"
+    else
+	git -C "${DIR}" pull
+    fi
+}
+
+get_or_sync libs/cmcstl2 https://github.com/CaseyCarter/cmcstl2.git 
+get_or_sync libs/GSL https://github.com/Microsoft/GSL.git
+get_or_sync libs/gsl-lite https://github.com/martinmoene/gsl-lite.git
+get_or_sync libs/opencv https://github.com/opencv/opencv.git
+get_or_sync libs/xtensor https://github.com/QuantStack/xtensor.git
+
+#########################
+
+#########################
+# D
+
+# MIR GLAS
+if [ ! -d "${OPT}/libs/d/mir-glas-trunk" ]; then
+    git clone https://github.com/libmir/mir-glas.git ${OPT}/libs/d/mir-glas-trunk
+    git -C ${OPT}/libs/d/mir-glas-trunk checkout master
+else
+    git -C ${OPT}/libs/d/mir-glas-trunk pull origin master
+fi
+install_mir_glas() {
+    # https://github.com/libmir/mir-glas/archive/v0.2.3.tar.gz
+    local VERSION=$1
+    local DEST=${OPT}/libs/d/mir-glas-v${VERSION}/
+    if [[ ! -d ${DEST} ]]; then
+        mkdir -p /tmp/mir-glas
+        pushd /tmp/mir-glas
+        fetch https://github.com/libmir/mir-glas/archive/v${VERSION}.tar.gz | tar zxf -
+        mkdir -p ${DEST}
+        rsync -a mir-glas-${VERSION}/ ${DEST}
+        popd
+        rm -rf /tmp/mir-glas
+    fi
+}
+install_mir_glas 0.1.5
+install_mir_glas 0.2.3
+
+# MIR Algorithm
+if [ ! -d "${OPT}/libs/d/mir-algorithm-trunk" ]; then
+    git clone https://github.com/libmir/mir-algorithm.git ${OPT}/libs/d/mir-algorithm-trunk
+    git -C ${OPT}/libs/d/mir-algorithm-trunk checkout master
+else
+    git -C ${OPT}/libs/d/mir-algorithm-trunk pull origin master
+fi
+install_mir_algorithm() {
+    # https://github.com/libmir/mir-algorithm/archive/v0.6.11.tar.gz
+    local VERSION=$1
+    local DEST=${OPT}/libs/d/mir-algorithm-v${VERSION}/
+    if [[ ! -d ${DEST} ]]; then
+        mkdir -p /tmp/mir-algorithm
+        pushd /tmp/mir-algorithm
+        fetch https://github.com/libmir/mir-algorithm/archive/v${VERSION}.tar.gz | tar zxf -
+        mkdir -p ${DEST}
+        rsync -a mir-algorithm-${VERSION}/ ${DEST}
+        popd
+        rm -rf /tmp/mir-algorithm
+    fi
+}
+install_mir_algorithm 0.5.17
+install_mir_algorithm 0.6.13
+
+#########################
+
