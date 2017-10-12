36a51cf413ba5c657b615e401e161408cf9839fb
Rubén <rubrinbla@gmail.com>
2017-10-12 23:46:37 +0200

    Add ranges-v3

--- update_compilers/install_libraries.sh
+++ update_compilers/install_libraries.sh
@@ -76,6 +76,7 @@ get_github_versioned_and_trunk() {
 
 get_github_versioned_and_trunk libs/ulib stefanocasazza/ULib v1.4.2
 get_github_versioned_and_trunk libs/google-benchmark google/benchmark v1.2.0
+get_github_versioned_and_trunk libs/rangesv3 ericniebler/range-v3 0.3.0
 
 # Eigen
 get_if_not_there libs/eigen/v3.3.4 http://bitbucket.org/eigen/eigen/get/3.3.4.tar.gz
