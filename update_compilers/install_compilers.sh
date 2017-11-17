25bbe1ca08645a03a61fd5aecd550e1532a6df8b
Matt Godbolt <matt@godbolt.org>
2017-11-17 11:16:05 -0600

    Fix swift install

--- update_compilers/install_compilers.sh
+++ update_compilers/install_compilers.sh
@@ -465,7 +465,7 @@ get_swift() {
         pushd ${DIR}
         fetch https://swift.org/builds/swift-${VER}-release/ubuntu1604/swift-${VER}-RELEASE/swift-${VER}-RELEASE-ubuntu16.04.tar.gz | tar zxf - --strip-components 1
         # work around insane installation issue
-        chmod og+r ${DIR}/usr/lib/swift/CoreFoundation/*
+        chmod og+r ./usr/lib/swift/CoreFoundation/*
         popd
     fi
 }
