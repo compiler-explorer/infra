1b494904aa5a8bae850aca37de6442ea61d57770
Matt Godbolt <mgodbolt@drw.com>
2017-11-22 10:21:57 -0600

    Argh

--- setup.fish
+++ setup.fish
@@ -1,5 +1,5 @@
 set fish_greeting
 
 if not echo $fish_user_paths | grep /home/ubuntu/compiler-explorer-image/bin
-    set -U fish_user_path $fish_user_paths /home/ubuntu/compiler-explorer-image/bin
+    set -U fish_user_paths $fish_user_paths /home/ubuntu/compiler-explorer-image/bin
 end
