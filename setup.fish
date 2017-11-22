7d45d2d98644f872f361525bbb1a7756ecef0303
Matt Godbolt <mgodbolt@drw.com>
2017-11-22 10:20:15 -0600

    Typo fix

--- setup.fish
+++ setup.fish
@@ -1,5 +1,5 @@
 set fish_greeting
 
-if not echo $fish_user_path | grep /home/ubuntu/compiler-explorer-image/bin
-    set -U fish_user_path $fish_user_path /home/ubuntu/compiler-explorer-image/bin
+if not echo $fish_user_paths | grep /home/ubuntu/compiler-explorer-image/bin
+    set -U fish_user_path $fish_user_paths /home/ubuntu/compiler-explorer-image/bin
 end
