80a0e29645df82bf114bfb30e20fa12de5ff7460
Matt Godbolt <matt@godbolt.org>
2017-12-10 09:16:50 -0600

    Fix for EBS optimized main CE node config

--- new_launch_config.sh
+++ new_launch_config.sh
@@ -9,7 +9,6 @@ COMMON_ARGS+="--associate-public-ip-address "
 COMMON_ARGS+="--iam-instance-profile XaniaBlog "
 COMMON_ARGS+="--security-groups sg-99df30fd "
 COMMON_ARGS+="--key-name mattgodbolt "
-COMMON_ARGS+="--ebs-optimized "
 COMMON_ARGS+="--block-device-mappings DeviceName=/dev/sda1,Ebs={VolumeSize=10,VolumeType=gp2,DeleteOnTermination=true} "
 COMMON_ARGS+="--instance-monitoring Enabled=False "
 
@@ -20,10 +19,12 @@ aws autoscaling create-launch-configuration --launch-configuration-name ${BASE_N
 aws autoscaling create-launch-configuration --launch-configuration-name ${BASE_NAME}-prod-c5 --image-id $1 \
     --instance-type c5.large \
     --spot-price 0.05 \
+    --ebs-optimized \
     ${COMMON_ARGS}
 
 aws autoscaling create-launch-configuration --launch-configuration-name ${BASE_NAME}-beta-c5 --image-id $1 \
     --instance-type c5.large \
     --spot-price 0.05 \
+    --ebs-optimized \
     --user-data "Beta" \
     ${COMMON_ARGS}
