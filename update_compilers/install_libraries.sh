6b6d2ca9860b798669ca9bca219d547526cc676e
RabsRincon <rubrinbla@gmail.com>
2018-03-08 20:58:57 +0100

    Refactor all libs installations to only use 1 fn

--- update_compilers/install_libraries.sh
+++ update_compilers/install_libraries.sh
@@ -7,8 +7,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 #########################
 # C++
-
-# add kvasir libraries
 if [ ! -d "libs/kvasir/mpl/trunk" ]; then
     git clone https://github.com/kvasir-io/mpl.git libs/kvasir/mpl/trunk
     git -C libs/kvasir/mpl/trunk checkout development
@@ -17,22 +15,22 @@ else
 fi
 
 install_boost() {
-    local VERSION=$1
-    local VERSION_UNDERSCORE=$(echo ${VERSION} | tr . _)
-    local DEST=${OPT}/libs/boost_${VERSION_UNDERSCORE}/boost/ 
-    if [[ ! -d ${DEST} ]]; then
-        mkdir -p /tmp/boost
-        pushd /tmp/boost
-        fetch https://dl.bintray.com/boostorg/release/${VERSION}/source/boost_${VERSION_UNDERSCORE}.tar.bz2 | tar jxf - boost_${VERSION_UNDERSCORE}/boost
-        mkdir -p ${OPT}/libs/boost_${VERSION_UNDERSCORE}/boost
-        rsync -a boost_${VERSION_UNDERSCORE}/boost/ ${DEST}
-        popd
-        rm -rf /tmp/boost
-    fi
+    for VERSION in "$@"; do
+        local VERSION_UNDERSCORE=$(echo ${VERSION} | tr . _)
+        local DEST=${OPT}/libs/boost_${VERSION_UNDERSCORE}/boost/
+        if [[ ! -d ${DEST} ]]; then
+            mkdir -p /tmp/boost
+            pushd /tmp/boost
+            fetch https://dl.bintray.com/boostorg/release/${VERSION}/source/boost_${VERSION_UNDERSCORE}.tar.bz2 | tar jxf - boost_${VERSION_UNDERSCORE}/boost
+            mkdir -p ${OPT}/libs/boost_${VERSION_UNDERSCORE}/boost
+            rsync -a boost_${VERSION_UNDERSCORE}/boost/ ${DEST}
+            popd
+            rm -rf /tmp/boost
+        fi
+    done
 }
-install_boost 1.64.0
-install_boost 1.65.0
-install_boost 1.66.0
+
+install_boost 1.64.0 1.65.0 1.66.0
 
 get_or_sync() {
     local DIR=$1
@@ -73,7 +71,6 @@ get_github_versioned_and_trunk_with_quirk() {
     shift 3
     mkdir -p $DIR
     get_or_sync ${DIR}/${QUIRK}trunk ${URL}.git
-    local version
     for tag in "$@"; do
         get_if_not_there ${DIR}/${QUIRK}${tag} ${URL}/archive/${tag}.tar.gz
     done
@@ -97,12 +94,12 @@ get_github_versioned_and_trunk libs/hfsm andrew-gresyk/HFSM 0.8
 get_github_versioned_and_trunk_with_quirk libs/eigen eigenteam/eigen-git-mirror v 3.3.4
 get_github_versioned_and_trunk libs/glm g-truc/glm 0.9.8.5
 
+
 #########################
 
 #########################
 # D
 
-# MIR GLAS
 if [ ! -d "${OPT}/libs/d/mir-glas-trunk" ]; then
     git clone https://github.com/libmir/mir-glas.git ${OPT}/libs/d/mir-glas-trunk
     git -C ${OPT}/libs/d/mir-glas-trunk checkout master
@@ -110,23 +107,22 @@ else
     git -C ${OPT}/libs/d/mir-glas-trunk pull origin master
 fi
 install_mir_glas() {
-    # https://github.com/libmir/mir-glas/archive/v0.2.3.tar.gz
-    local VERSION=$1
-    local DEST=${OPT}/libs/d/mir-glas-v${VERSION}/
-    if [[ ! -d ${DEST} ]]; then
-        mkdir -p /tmp/mir-glas
-        pushd /tmp/mir-glas
-        fetch https://github.com/libmir/mir-glas/archive/v${VERSION}.tar.gz | tar zxf -
-        mkdir -p ${DEST}
-        rsync -a mir-glas-${VERSION}/ ${DEST}
-        popd
-        rm -rf /tmp/mir-glas
-    fi
+    for VERSION in "$@"; do
+        local DEST=${OPT}/libs/d/mir-glas-v${VERSION}/
+        if [[ ! -d ${DEST} ]]; then
+            mkdir -p /tmp/mir-glas
+            pushd /tmp/mir-glas
+            fetch https://github.com/libmir/mir-glas/archive/v${VERSION}.tar.gz | tar zxf -
+            mkdir -p ${DEST}
+            rsync -a mir-glas-${VERSION}/ ${DEST}
+            popd
+            rm -rf /tmp/mir-glas
+        fi
+    done
 }
-install_mir_glas 0.1.5
-install_mir_glas 0.2.3
 
-# MIR Algorithm
+install_mir_glas 0.1.5 0.2.3
+
 if [ ! -d "${OPT}/libs/d/mir-algorithm-trunk" ]; then
     git clone https://github.com/libmir/mir-algorithm.git ${OPT}/libs/d/mir-algorithm-trunk
     git -C ${OPT}/libs/d/mir-algorithm-trunk checkout master
@@ -134,22 +130,19 @@ else
     git -C ${OPT}/libs/d/mir-algorithm-trunk pull origin master
 fi
 install_mir_algorithm() {
-    # https://github.com/libmir/mir-algorithm/archive/v0.6.11.tar.gz
-    local VERSION=$1
-    local DEST=${OPT}/libs/d/mir-algorithm-v${VERSION}/
-    if [[ ! -d ${DEST} ]]; then
-        mkdir -p /tmp/mir-algorithm
-        pushd /tmp/mir-algorithm
-        fetch https://github.com/libmir/mir-algorithm/archive/v${VERSION}.tar.gz | tar zxf -
-        mkdir -p ${DEST}
-        rsync -a mir-algorithm-${VERSION}/ ${DEST}
-        popd
-        rm -rf /tmp/mir-algorithm
-    fi
+    for VERSION in "$@"; do
+        local VERSION=$1
+        local DEST=${OPT}/libs/d/mir-algorithm-v${VERSION}/
+        if [[ ! -d ${DEST} ]]; then
+            mkdir -p /tmp/mir-algorithm
+            pushd /tmp/mir-algorithm
+            fetch https://github.com/libmir/mir-algorithm/archive/v${VERSION}.tar.gz | tar zxf -
+            mkdir -p ${DEST}
+            rsync -a mir-algorithm-${VERSION}/ ${DEST}
+            popd
+            rm -rf /tmp/mir-algorithm
+        fi
+    done
 }
-install_mir_algorithm 0.5.17
-install_mir_algorithm 0.6.13
-install_mir_algorithm 0.6.21
 
-#########################
-
+install_mir_algorithm 0.5.17 0.6.13 0.6.21
