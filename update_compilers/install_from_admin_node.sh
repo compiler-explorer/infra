0233992899c188c4d36527c760ba742a1d9e0c74
Matt Godbolt <matt@godbolt.org>
2018-03-14 22:04:10 -0500

    Make life easier...hopefully

--- update_compilers/install_from_admin_node.sh
+++ update_compilers/install_from_admin_node.sh
@@ -2,6 +2,9 @@
 
 set -ex
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd ${SCRIPT_DIR}
+
 sudo ./install_binaries.sh
 sudo ./install_compilers.sh nightly
 sudo ./install_nonfree_compilers.sh
