819bd599332476c3c2fc410fe34ea767ada47cd1
Matt Godbolt <matt@godbolt.org>
2018-05-07 18:58:47 -0500

    Install NVIDIA CUDA 9.1.85

--- update_compilers/install_nonfree_compilers.sh
+++ update_compilers/install_nonfree_compilers.sh
@@ -97,3 +97,24 @@ for version in 20170226-190308-1.0; do
         s3get ${S3URL}/zapcc-key.txt ${OPT}/${fullname}/bin/zapcc-key.txt
     fi
 done
+
+##################################
+# CUDA
+install_cuda() {
+    local URL=$1
+    mkdir -p cuda
+    pushd cuda
+    local DIR=$(pwd)/$2
+    if [[ ! -d ${DIR} ]]; then
+      rm -rf /tmp/cuda
+      mkdir /tmp/cuda
+      fetch ${URL} > /tmp/cuda/combined.sh
+      sh /tmp/cuda/combined.sh --extract=/tmp/cuda
+      local LINUX=$(ls -1 /tmp/cuda/cuda-linux.$2*.run)
+      ${LINUX} --prefix=${DIR} -noprompt -nosymlink -no-man-page
+      rm -rf /tmp/cuda
+    fi
+    popd
+}
+
+install_cuda https://developer.nvidia.com/compute/cuda/9.1/Prod/local_installers/cuda_9.1.85_387.26_linux 9.1.85
