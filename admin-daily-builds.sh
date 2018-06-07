5ada86685821b487218b173faed8c5bf2defe4bd
Matt Godbolt <matt@godbolt.org>
2018-06-07 08:39:32 -0500

    Update admin daily build

--- admin-daily-builds.sh
+++ admin-daily-builds.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -ex
+set -exuo pipefail
 
 finish() {
 	ce builder stop
@@ -10,16 +10,18 @@ trap finish EXIT
 ce builder status
 ce builder start
 
-
 FAILED=0
 run_on_build() {
-	ce builder exec $* || FAILED=1
+    set +e
+	if ! ce builder exec "$@"; then
+	    FAILED=1
+    fi
+	set -e
 }
 
 mkdir -p ~/build_logs
-run_on_build sudo compiler-explorer-image/build_and_upload_latest_gcc.sh | tee ~/build_logs/gcc
-run_on_build sudo compiler-explorer-image/build_and_upload_latest_clang.sh | tee ~/build_logs/clang
-run_on_build sudo compiler-explorer-image/build_and_upload_latest_clang_concepts.sh | tee ~/build_logs/clang_concepts
-run_on_build sudo compiler-explorer-image/build_and_upload_latest_clang_cppx.sh | tee ~/build_logs/clang_cppx
-
-exit $FAILED
+run_on_build sudo compiler-explorer-image/build_and_upload_latest_gcc.sh 2>&1 | tee ~/build_logs/gcc
+run_on_build sudo compiler-explorer-image/build_and_upload_latest_clang.sh 2>&1 | tee ~/build_logs/clang
+run_on_build sudo compiler-explorer-image/build_and_upload_latest_clang_concepts.sh 2>&1 | tee ~/build_logs/clang_concepts
+run_on_build sudo compiler-explorer-image/build_and_upload_latest_clang_cppx.sh 2>&1 | tee ~/build_logs/clang_cppx
+exit ${FAILED}
