e0f38a0f9eb1baeeaeeda0aebd3546e6f3823b32
Matt Godbolt <matt@godbolt.org>
2018-09-29 17:51:30 -0500

    Add the CE VPC

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "compiler-explorer"
-    key    = "terraform/terraform.tfstate"
+    key = "terraform/terraform.tfstate"
     region = "us-east-1"
   }
 }
