3dd34813453177162da3cb9b0f0dcede4a34b786
Partouf <partouf@gmail.com>
2018-10-06 16:54:53 +0200

    see if we can get 32 bit compilers

--- update_compilers/install_clean_compilers.sh
+++ update_compilers/install_clean_compilers.sh
@@ -3,10 +3,23 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . ${SCRIPT_DIR}/common.inc
 
-get_clean() {
+get_clean32() {
 	local VER=$1
 	local VERNODOTS=$2
-	local DIR=clean-$VER
+	local DIR=clean64-$VER
+
+	if [[ ! -d ${DIR} ]]; then
+		mkdir ${DIR}
+		pushd ${DIR}
+		fetch https://ftp.cs.ru.nl/Clean/Clean${VERNODOTS}/linux/clean${VER}_32.tar.gz | tar xzf - --strip-components 1
+		popd
+	fi
+}
+
+get_clean64() {
+	local VER=$1
+	local VERNODOTS=$2
+	local DIR=clean32-$VER
 
 	if [[ ! -d ${DIR} ]]; then
 		mkdir ${DIR}
@@ -16,6 +29,8 @@ get_clean() {
 	fi
 }
 
-get_clean 2.4 24
-get_clean 3.0 30
+get_clean32 2.4 24
+get_clean32 3.0 30
 
+get_clean64 2.4 24
+get_clean64 3.0 30
