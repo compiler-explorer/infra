8a5f47799b7f87b365b299f8eaf88b87b4fa8200
John McFarlane <github@john.mcfarlane.name>
2018-10-21 12:02:55 +0100

    Add CNL library to C++

--- update_compilers/install_libraries.sh
+++ update_compilers/install_libraries.sh
@@ -112,6 +112,7 @@ get_or_sync libs/abseil https://github.com/abseil/abseil-cpp.git
 get_or_sync libs/cppcoro https://github.com/lewissbaker/cppcoro.git
 get_or_sync libs/ctbignum https://github.com/niekbouman/ctbignum.git
 get_or_sync libs/outcome https://github.com/ned14/outcome.git
+get_or_sync libs/cnl https://github.com/johnmcfarlane/cnl.git
 
 get_if_not_there() {
     local DIR=$1
