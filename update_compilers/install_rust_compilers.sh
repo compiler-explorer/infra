79ecef71dece894d72caabd8d944622db2f923bf
Matt Godbolt <matt@godbolt.org>
2018-10-31 12:12:58 -0500

    Install rust 1.30

--- update_compilers/install_rust_compilers.sh
+++ update_compilers/install_rust_compilers.sh
@@ -202,6 +202,7 @@ install_new_rust 1.27.0 RUST_TARGETS[@]
 install_new_rust 1.27.1 RUST_TARGETS[@]
 install_new_rust 1.28.0 RUST_TARGETS[@]
 install_new_rust 1.29.0 RUST_TARGETS[@]
+install_new_rust 1.30.0 RUST_TARGETS[@]
 if install_nightly; then
     RUST_TARGETS+=(
         riscv32imac-unknown-none-elf
