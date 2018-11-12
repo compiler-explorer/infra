199420e1a9d95d0fbafc4a368fae84a0152903f1
Partouf <partouf@gmail.com>
2018-11-12 22:26:56 +0100

    Logging

--- init/stop.sh
+++ init/stop.sh
@@ -7,5 +7,9 @@ fi
 
 . /compiler-explorer-image/init/shared.sh
 
+echo "Stopping containers"
 $SUDO docker stop ${CE_ALL_CONTAINERS} || true
+
+echo "Removing containers"
 $SUDO docker rm ${CE_ALL_CONTAINERS} || true
+
