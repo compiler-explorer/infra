91cacbc66653114460250d76e724e529c9615cba
Kale Blankenship <kale@lemnisys.com>
2018-11-15 19:58:53 -0800

    Add Go daily builds

--- update_compilers/install_go_compilers.sh
+++ update_compilers/install_go_compilers.sh
@@ -14,7 +14,7 @@ install_golang() {
     local VERSION=$1
     local DIR=golang-${VERSION}
     if [[ -d ${DIR} ]]; then
-        echo Golang ${VERSION} already intalled, skipping
+        echo Golang ${VERSION} already installed, skipping
         return
     fi
     mkdir ${DIR}
@@ -22,6 +22,36 @@ install_golang() {
     fetch https://storage.googleapis.com/golang/go${VERSION}.linux-amd64.tar.gz | tar zxf -
     popd
     do_strip ${DIR}
+}
+
+do_nightly_install() {
+    local COMPILER_PATTERN="$1"
+    local DESTINATION="$2"
+    # work around a cronic issue where the execution output is interpreted as error
+    # if it spans multiple lines: assigning output with multiple lines to a variable
+    # fools it.
+    set +x
+    compilers=$(echo $ALL_COMPILERS | grep -oE "${COMPILER_PATTERN}-[0-9]+" | sort)
+    set -x
+    compiler_array=(${compilers})
+    latest=${compiler_array[-1]}
+    # Extract the latest...
+    if [[ ! -d ${latest} ]]; then
+        fetch ${S3URL}/${latest}.tar.xz > latest.xz
+        tar Jxf latest.xz
+        rm latest.xz
+    fi
+    # Ensure the symlink points at the latest
+    rm -f ${OPT}/${DESTINATION}
+    ln -s ${latest} ${OPT}/${DESTINATION}
+    # Clean up any old snapshots
+    for compiler in ${COMPILER_PATTERN}-[0-9]*; do
+        if [[ -d ${compiler} ]]; then
+            if [[ "${compiler}" != "${latest}" ]]; then
+                rm -rf ${compiler}
+            fi
+        fi
+    done
 }
 
 install_golang 1.7.2
@@ -32,3 +62,7 @@ install_golang 1.9.4
 install_golang 1.10
 install_golang 1.10.1
 install_golang 1.11
+
+if install_nightly; then
+    do_nightly_install go-trunk go-tip
+fi
