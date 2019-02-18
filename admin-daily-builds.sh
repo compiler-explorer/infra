1c5d550e15c6723ab910bb36daf332bc1417f545
Matt Godbolt <matt@godbolt.org>
2019-02-17 19:58:24 -0600

    Build contracts compiler daily

--- admin-daily-builds.sh
+++ admin-daily-builds.sh
@@ -31,22 +31,24 @@ run_on_build() {
 build_latest() {
     local IMAGE=$1
     local BUILD_NAME=$2
-    shift 2
+    local COMMAND=$3
+    local BUILD=$4
     run_on_build ${BUILD_NAME} \
       sudo docker run --rm --name ${BUILD_NAME}.build -v/home/ubuntu/.s3cfg:/root/.s3cfg:ro -e 'LOGSPOUT=ignore' \
       mattgodbolt/${IMAGE}-builder \
-      bash "$@" trunk s3://compiler-explorer/opt/
+      bash "${COMMAND}" ${BUILD} s3://compiler-explorer/opt/
     log_to_json ${LOG_DIR} admin/buildStatus.json
 }
 
-build_latest gcc gcc build.sh
-build_latest clang clang build.sh
-build_latest clang clang_concepts build-concepts.sh
-build_latest clang clang_cppx build-cppx.sh
-build_latest clang clang_relocatable build-relocatable.sh
-build_latest clang clang_autonsdmi build-autonsdmi.sh
-build_latest clang clang_lifetime build-lifetime.sh
-build_latest clang clang_parmexpr build-parmexpr.sh
-build_latest go go build.sh
+build_latest gcc gcc build.sh trunk
+build_latest gcc gcc build.sh lock3-contracts-trunk
+build_latest clang clang build.sh trunk
+build_latest clang clang_concepts build-concepts.sh trunk
+build_latest clang clang_cppx build-cppx.sh trunk
+build_latest clang clang_relocatable build-relocatable.sh trunk
+build_latest clang clang_autonsdmi build-autonsdmi.sh trunk
+build_latest clang clang_lifetime build-lifetime.sh trunk
+build_latest clang clang_parmexpr build-parmexpr.sh trunk
+build_latest go go build.sh trunk
 
 exit ${BUILD_FAILED}
