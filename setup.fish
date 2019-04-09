eeb534a630a5d2ffd1a24af99f0b4b01b03124e5
Matt Godbolt <matt@godbolt.org>
2019-04-08 21:18:00 -0500

    More tweaks

--- setup.fish
+++ setup.fish
@@ -7,13 +7,13 @@ end
 set -Ux EDITOR /usr/bin/vim
 
 mkdir -p ~/.config/fish/
-echo begin >> ~/.config/fish/config.fish
+echo begin > ~/.config/fish/config.fish
 echo   set --local AUTOJUMP_PATH /usr/share/autojump/autojump.fish >> ~/.config/fish/config.fish
 echo   if test -e \$AUTOJUMP_PATH >> ~/.config/fish/config.fish
 echo     source \$AUTOJUMP_PATH >> ~/.config/fish/config.fish
 echo   end >> ~/.config/fish/config.fish
 echo end >> ~/.config/fish/config.fish
 echo set VIRTUAL_ENV_DISABLE_PROMPT yes >> ~/.config/fish/config.fish
-echo if test -e ~/compiler-explorer-image/bin/activate-fish >> ~/.config/fish/config.fish
-echo   source ~/compiler-explorer-image/bin/activate.fish >> ~/.config/fish/config.fish
+echo if test -e ~/compiler-explorer-image/.env/activate-fish >> ~/.config/fish/config.fish
+echo   source ~/compiler-explorer-image/.env/activate.fish >> ~/.config/fish/config.fish
 echo end >> ~/.config/fish/config.fish
