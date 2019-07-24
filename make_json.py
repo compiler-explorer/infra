e3f115d4557c00ee168136e8ee221894caab8c28
Austin Morton <austinpmorton@gmail.com>
2019-07-24 01:31:05 -0400

    Use python3 for make_json.py

--- make_json.py
+++ make_json.py
@@ -1,12 +1,12 @@
-import ConfigParser
+from configparser import ConfigParser
 import os
 import json
 
 obj = {}
-config = ConfigParser.ConfigParser()
+config = ConfigParser()
 config.read(os.path.join(os.getenv("HOME"), ".aws", "credentials"))
-obj["MY_ACCESS_KEY"] = config.get("default", "aws_access_key_id", "")
-obj["MY_SECRET_KEY"] = config.get("default", "aws_secret_access_key", "")
+obj["MY_ACCESS_KEY"] = config.get("default", "aws_access_key_id", fallback="")
+obj["MY_SECRET_KEY"] = config.get("default", "aws_secret_access_key", fallback="")
 
 with open("config.json", "w") as out:
     json.dump(obj, out)
