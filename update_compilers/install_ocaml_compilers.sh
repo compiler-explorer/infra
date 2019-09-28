a2ea5211b90d89c8a781eb40d7128595a96e8d37
freyr <sky_rider_93@mail.ru>
2019-09-28 20:08:44 +0300

    OCaml: add 4.09, upgrade 4.08.1

--- update_compilers/install_ocaml_compilers.sh
+++ update_compilers/install_ocaml_compilers.sh
@@ -14,7 +14,9 @@ get_ocaml() {
 get_ocaml 4.04.2
 get_ocaml 4.06.1
 get_ocaml 4.07.1
-get_ocaml 4.08.0
+get_ocaml 4.08.1
+get_ocaml 4.09.0
 
 get_ocaml 4.07.1-flambda
-get_ocaml 4.08.0-flambda
+get_ocaml 4.08.1-flambda
+get_ocaml 4.09.0-flambda
