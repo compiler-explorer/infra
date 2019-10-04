9e68a3e37a50d4446869543eb473f04f4fa7c254
Matt Godbolt <matt@godbolt.org>
2019-10-04 07:27:25 -0500

    Mount `/opt/arm` too

--- init/start.sh
+++ init/start.sh
@@ -21,6 +21,9 @@ mount_opt() {
     mkdir -p /opt/intel
     mountpoint /opt/intel || mount --bind /efs/intel /opt/intel
 
+    mkdir -p /opt/arm
+    mountpoint /opt/arm || mount --bind /efs/arm /opt/arm
+
     [ -f /opt/.health ] || touch /opt/.health
     mountpoint /opt/.health || mount --bind /efs/.health /opt/.health
 }
