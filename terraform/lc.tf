9653af32e353500562a99bf212f62ed05c117cf9
Patrick Quist <partouf@gmail.com>
2019-11-15 14:22:03 +0100

    new ami

--- terraform/lc.tf
+++ terraform/lc.tf
@@ -1,7 +1,7 @@
 locals {
-  image_id          = "ami-0b7b4f53cebc34695"
-  staging_image_id  = "ami-0b7b4f53cebc34695"
-  beta_image_id     = "ami-0b7b4f53cebc34695"
+  image_id          = "ami-0cea43007637c56d9"
+  staging_image_id  = "ami-0cea43007637c56d9"
+  beta_image_id     = "ami-0cea43007637c56d9"
   staging_user_data = base64encode("staging")
   beta_user_data    = base64encode("beta")
   // Current c5 on-demand price is 0.085. Yearly pre-pay is 0.05 (so this is same as prepaying a year)
