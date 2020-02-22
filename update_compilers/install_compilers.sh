f69663b8ea02cb7de54614f540be19985ea29a3b
Rune Tynan <runetynan@gmail.com>
2020-02-22 14:41:53 -0500

    Add Python to install_compilers.sh

--- update_compilers/install_compilers.sh
+++ update_compilers/install_compilers.sh
@@ -73,3 +73,7 @@ ce_install 'compilers/c++'
 #########################
 # Nim
 . ${SCRIPT_DIR}/install_nim_compilers.sh ${ARG1}
+
+#########################
+# Python
+. ${SCRIPT_DIR}/install_python_compilers.sh ${ARG1}
