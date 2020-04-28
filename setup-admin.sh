9d1eaf12382ac37c0b0a0d1f73b37178ad982db0
Matt Godbolt <matt@godbolt.org>
2020-04-27 22:40:12 -0500

    Move to python3.8

--- setup-admin.sh
+++ setup-admin.sh
@@ -14,7 +14,7 @@ fi
 
 env EXTRA_NFS_ARGS="" ${DIR}/setup-common.sh
 
-apt -y install python2.7 python-pip mosh fish jq ssmtp cronic subversion upx gdb autojump zlib1g-dev m4 python3 python3-venv python3-pip libc6-dev-i386
+apt -y install python2.7 python-pip mosh fish jq ssmtp cronic subversion upx gdb autojump zlib1g-dev m4 python3 python3-venv python3-pip python3.8 python3.8-venv libc6-dev-i386
 chsh ubuntu -s /usr/bin/fish
 
 cd /home/ubuntu/compiler-explorer-image
