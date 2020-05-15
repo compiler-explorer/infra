7430f64622b5b1b3942728519a44fd12944a349b
Partouf <partouf@gmail.com>
2020-05-15 21:38:44 +0200

    use logged revision by docker buildscript instead

--- admin-daily-builds.sh
+++ admin-daily-builds.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-mkdir -p /opt/compiler-explorer/buildrevs
-
 set -exuo pipefail
 
 finish() {
@@ -35,64 +33,35 @@ build_latest() {
     local BUILD_NAME=$2
     local COMMAND=$3
     local BUILD=$4
-    local GITURL=$5
-    local GITBRANCH=$6
-    local SHOULDBUILD=0
-    local LATESTREV="Unknown"
-    local FILEWITHHASH="/opt/compiler-explorer/buildrevs/${IMAGE}-${BUILD_NAME}-${BUILD}.txt"
 
-    if [[ "${GITURL}" != "" ]]; then
-        LATESTREV=$(git ls-remote --heads ${GITURL} ${GITBRANCH})
-        BUILTREV=""
-        if [ -f "${FILEWITHHASH}" ]; then
-            BUILTREV=$(cat ${FILEWITHHASH})
-        fi
-
-        if [ "${BUILTREV}" != "${LATESTREV}" ]; then
-            SHOULDBUILD=1
-        fi
-    else
-        SHOULDBUILD=1
-    fi
-
-    if [ $SHOULDBUILD -eq 1 ]; then
-        echo "Starting build ${BUILD_NAME}"
+    local logdir=${LOG_DIR}/$1
+    local LATESTCEREV=$(cat ${logdir}/log | grep -P "^ce-build-revision\:.*" | cut -d ':' -f 2)
 
         run_on_build "${BUILD_NAME}" \
             sudo docker run --rm --name "${BUILD_NAME}.build" -v/home/ubuntu/.s3cfg:/root/.s3cfg:ro -e 'LOGSPOUT=ignore' \
             "compilerexplorer/${IMAGE}-builder" \
-            bash "${COMMAND}" "${BUILD}" s3://compiler-explorer/opt/
+        bash "${COMMAND}" "${BUILD}" s3://compiler-explorer/opt/ "${LATESTCEREV}"
         log_to_json ${LOG_DIR} admin
-        
-        if [[ ${BUILD_FAILED} -eq 0 ]]; then
-            echo "Writing hash to file"
-            echo "${LATESTREV}" > ${FILEWITHHASH}
-        fi
-    fi
 }
 
+
+
 # llvm build is fast, so lets do it first
-build_latest clang llvm build-llvm.sh trunk https://github.com/llvm/llvm-project.git master
+build_latest clang llvm build-llvm.sh trunk
 
-build_latest gcc gcc build.sh trunk git://gcc.gnu.org/git/gcc.git master
-build_latest gcc gcc_contracts build.sh lock3-contracts-trunk https://gitlab.com/lock3/gcc-new.git contracts
-build_latest gcc gcc_modules build.sh cxx-modules-trunk git://gcc.gnu.org/git/gcc.git devel/c++-modules
-build_latest gcc gcc_coroutines build.sh cxx-coroutines-trunk git://gcc.gnu.org/git/gcc.git devel/c++-coroutines
-build_latest gcc gcc_embed build.sh embed-trunk https://github.com/ThePhD/gcc.git feature/embed
-build_latest gcc gcc_static_analysis build.sh static-analysis-trunk git://gcc.gnu.org/git/gcc.git devel/analyzer
-build_latest clang clang build.sh trunk https://github.com/llvm/llvm-project.git master
-
-# gitlab doesn't support ls-remote without logging in apparantly
-build_latest clang clang_cppx build-cppx.sh trunk "" ""
-
-build_latest clang clang_relocatable build-relocatable.sh trunk https://github.com/Quuxplusone/llvm-project.git trivially-relocatable
-build_latest clang clang_autonsdmi build-autonsdmi.sh trunk https://github.com/cor3ntin/llvm-project experiments
-build_latest clang clang_lifetime build-lifetime.sh trunk https://github.com/mgehre/llvm-project lifetime
-
-# no idea how this works
-build_latest clang clang_parmexpr build-parmexpr.sh trunk "" ""
-
-build_latest clang clang_embed build-embed.sh embed-trunk https://github.com/ThePhD/llvm-project.git feature/embed
-build_latest go go build.sh trunk https://go.googlesource.com/go master
+build_latest gcc gcc build.sh trunk
+build_latest gcc gcc_contracts build.sh lock3-contracts-trunk
+build_latest gcc gcc_modules build.sh cxx-modules-trunk
+build_latest gcc gcc_coroutines build.sh cxx-coroutines-trunk
+build_latest gcc gcc_embed build.sh embed-trunk
+build_latest gcc gcc_static_analysis build.sh static-analysis-trunk
+build_latest clang clang build.sh trunk
+build_latest clang clang_cppx build-cppx.sh trunk
+build_latest clang clang_relocatable build-relocatable.sh trunk
+build_latest clang clang_autonsdmi build-autonsdmi.sh trunk
+build_latest clang clang_lifetime build-lifetime.sh trunk
+build_latest clang clang_parmexpr build-parmexpr.sh trunk
+build_latest clang clang_embed build-embed.sh embed-trunk
+build_latest go go build.sh trunk
 
 exit ${BUILD_FAILED}
