d325921545bf1e0690ed0d158d3e26156d526217
RabsRincon <ruben@rinconblanco.es>
2020-05-25 14:16:48 +0200

    Install CUDA 10.2.89

--- update_compilers/install_nonfree_compilers.sh
+++ update_compilers/install_nonfree_compilers.sh
@@ -111,3 +111,4 @@ install_cuda https://developer.nvidia.com/compute/cuda/10.0/Prod/local_installer
 install_cuda https://developer.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda_10.1.105_418.39_linux.run 10.1.105
 install_cuda https://developer.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda_10.1.168_418.67_linux.run 10.1.168
 install_cuda https://developer.download.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda_10.1.243_418.87.00_linux.run 10.1.243
+install_cuda https://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda_10.2.89_440.33.01_linux.run 10.2.89
