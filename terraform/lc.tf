a42bd27ae38a9ff027d7e7e866e31c57b4424b9d
Partouf <partouf@gmail.com>
2020-06-11 01:09:48 +0200

    uptodateami

--- terraform/lc.tf
+++ terraform/lc.tf
@@ -2,7 +2,7 @@ locals {
   image_id          = "ami-08823b7a4d6a7afb1"
   staging_image_id  = "ami-08823b7a4d6a7afb1"
   beta_image_id     = "ami-08823b7a4d6a7afb1"
-  conan_image_id    = "ami-0eb9bb9ef44752a70"
+  conan_image_id    = "ami-0bd35fea46ffb0675"
   staging_user_data = base64encode("staging")
   beta_user_data    = base64encode("beta")
   // Current c5 on-demand price is 0.085. Yearly pre-pay is 0.05 (so this is same as prepaying a year)
