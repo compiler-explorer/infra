571271c959b1794a9bb715283f7c9a752fd53958
Partouf <partouf@gmail.com>
2020-06-11 18:20:52 +0200

    unused

--- setup-conanserver.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-sudo -u ce mkdir /home/ce/.conan_server
-echo "/dev/data/datavol       /home/ce/.conan_server   ext4   defaults,user=ce                0 0" >> /etc/fstab
-
-mount -a
-
-sudo -u ce /home/ce/.local/bin/gunicorn -b 0.0.0.0:9300 -w 4 -t 300 conans.server.server_launcher:app
