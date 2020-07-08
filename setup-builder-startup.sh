00466b77fd4123bb1b612a4ffc5e2440e60daf63
Austin Morton <austinpmorton@gmail.com>
2020-07-08 18:56:14 -0400

    Enable swap on builder for real this time?

--- setup-builder-startup.sh
+++ setup-builder-startup.sh
@@ -9,12 +9,12 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 systemctl stop docker
 umount /ephemeral || true
 sfdisk -f /dev/nvme0n1 <<EOF
-label: dos                                                                                                                                                                                                  │
-label-id: 0x7d7e658b                                                                                                                                                                                        │
-device: /dev/nvme0n1                                                                                                                                                                                        │
-unit: sectors                                                                                                                                                                                               │
-                                                                                                                                                                                                            │
-/dev/nvme0n1p1 : start=     3999744, size=   120999936, type=82                                                                                                                                             │
+label: dos
+label-id: 0x7d7e658b
+device: /dev/nvme0n1
+unit: sectors
+
+/dev/nvme0n1p1 : start=     3999744, size=   120999936, type=82
 /dev/nvme0n1p2 : start=   124999680, size=   656248832, type=83
 EOF
 sync
