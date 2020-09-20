c71e63227e4de8a6c7e7fd65544ad159f5e86521
Matt Godbolt <matt@godbolt.org>
2020-09-20 17:40:12 -0500

    Adopt the 5xx alarms (and make one for each subdomain)

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -2,6 +2,10 @@ data "aws_sns_topic" "alert" {
   name = "CompilerExplorerAlert"
 }
 
+locals {
+  approx_monthly_budget = 550
+}
+
 resource "aws_cloudwatch_metric_alarm" "resp_90ile_15m_too_slow" {
   alarm_name          = "90ile_15m_resp_too_slow"
   alarm_description   = "Monitor site response time, 90%ile being too slow"
@@ -17,5 +21,78 @@ resource "aws_cloudwatch_metric_alarm" "resp_90ile_15m_too_slow" {
     LoadBalancer = aws_alb.GccExplorerApp.arn_suffix
   }
   comparison_operator = "GreaterThanThreshold"
+  alarm_actions       = [data.aws_sns_topic.alert.arn]
+}
+
+resource "aws_cloudwatch_metric_alarm" "halfway_budget" {
+  alarm_name          = "HalfBudget"
+  alarm_description   = "A heads up we're halfway through the budget"
+  threshold           = local.approx_monthly_budget / 2
+  period              = 6 * 60 * 60
+  evaluation_periods  = 1
+  namespace           = "AWS/Billing"
+  metric_name         = "EstimatedCharges"
+  statistic           = "Maximum"
+  comparison_operator = "GreaterThanThreshold"
+  dimensions          = {
+    Currency = "USD"
+  }
+  alarm_actions       = [data.aws_sns_topic.alert.arn]
+}
+
+resource "aws_cloudwatch_metric_alarm" "budget_hit" {
+  alarm_name          = "BudgetHit"
+  alarm_description   = "We've hit the budget"
+  threshold           = local.approx_monthly_budget
+  period              = 6 * 60 * 60
+  evaluation_periods  = 1
+  namespace           = "AWS/Billing"
+  metric_name         = "EstimatedCharges"
+  comparison_operator = "GreaterThanThreshold"
+  statistic           = "Maximum"
+  dimensions          = {
+    Currency = "USD"
+  }
+  alarm_actions       = [data.aws_sns_topic.alert.arn]
+}
+
+resource "aws_cloudwatch_metric_alarm" "bankrupcy" {
+  alarm_name          = "EmergencyBillingAlert"
+  alarm_description   = "Is Matt about to have to sell his house?"
+  threshold           = local.approx_monthly_budget * 1.5
+  period              = 6 * 60 * 60
+  evaluation_periods  = 1
+  namespace           = "AWS/Billing"
+  metric_name         = "EstimatedCharges"
+  comparison_operator = "GreaterThanThreshold"
+  statistic           = "Maximum"
+  dimensions          = {
+    Currency = "USD"
+  }
+  alarm_actions       = [data.aws_sns_topic.alert.arn]
+}
+
+resource "aws_cloudwatch_metric_alarm" "cloudfront_high_5xx" {
+  for_each            = {
+    "godbolt.org"           = aws_cloudfront_distribution.ce-godbolt-org,
+    "compiler-explorer.com" = aws_cloudfront_distribution.compiler-explorer-com,
+    "godbo.lt"              = aws_cloudfront_distribution.godbo-lt,
+    "ce.cdn.net"            = aws_cloudfront_distribution.static-ce-cdn-net
+  }
+  alarm_name          = "cloudfront_high_5xx_${each.key}"
+  alarm_description   = "Unnacceptable level of 5xx errors on ${each.key}"
+  evaluation_periods  = 4
+  datapoints_to_alarm = 4
+  threshold           = 3
+  metric_name         = "5xxErrorRate"
+  namespace           = "AWS/CloudFront"
+  statistic           = "Average"
+  period              = 5 * 60
+
+  dimensions          = {
+    DistributionId = each.value.id
+    Region         = "Global"
+  }
+  comparison_operator = "GreaterThanOrEqualToThreshold"
   alarm_actions       = [data.aws_sns_topic.alert.arn]
 }
