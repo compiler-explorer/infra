c5a84a3558dd78e036eb9c07fa904e4cd2770442
JPailleux <jean-di.pailleux@outlook.com>
2020-09-28 19:48:42 +0200

    Update install_libraries.sh

--- update_compilers/install_libraries.sh
+++ update_compilers/install_libraries.sh
@@ -69,10 +69,10 @@ update_boost_archive() {
 #
 # See: https://github.com/mattgodbolt/compiler-explorer/issues/1771
 
-#install_boost 1.64.0 1.65.0 1.66.0 1.67.0 1.68.0 1.69.0 1.70.0 1.71.0 1.72.0 1.73.0 1.74.0
-#update_boost_archive
+install_boost 1.64.0 1.65.0 1.66.0 1.67.0 1.68.0 1.69.0 1.70.0 1.71.0 1.72.0 1.73.0 1.74.0
+update_boost_archive
 
-#ce_install 'libraries'
+ce_install 'libraries'
 
 #########################
 # OpenSSL
@@ -103,7 +103,7 @@ install_openssl() {
     done
 }
 
-#install_openssl 1_1_1c 1_1_1g
+install_openssl 1_1_1c 1_1_1g
 
 #########################
 # cs50
@@ -142,7 +142,7 @@ install_cs50_v9() {
     done
 }
 
-#install_cs50_v9 9.1.0
+install_cs50_v9 9.1.0
 
 
 #########################
@@ -240,7 +240,7 @@ install_lua() {
     done
 }
 
-#install_lua v5.3.5 v5.4.0
+install_lua v5.3.5 v5.4.0
 
 install_nsimd() {
     for VERSION in "$@"; do
