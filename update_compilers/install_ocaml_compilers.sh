15d4f86681ab6a0e9be6de085cbf919734bdeab8
Sora Morimoto <sora@morimoto.io>
2020-10-16 17:27:08 +0900

    Undo the removal of the old versions
    Signed-off-by: Sora Morimoto <sora@morimoto.io>

--- update_compilers/install_ocaml_compilers.sh
+++ update_compilers/install_ocaml_compilers.sh
@@ -15,12 +15,16 @@ get_ocaml 4.04.2
 get_ocaml 4.06.1
 get_ocaml 4.07.1
 get_ocaml 4.08.1
+get_ocaml 4.09.0
 get_ocaml 4.09.1
+get_ocaml 4.10.0
 get_ocaml 4.10.1
 get_ocaml 4.11.1
 
 get_ocaml 4.07.1-flambda
 get_ocaml 4.08.1-flambda
+get_ocaml 4.09.0-flambda
 get_ocaml 4.09.1-flambda
+get_ocaml 4.10.0-flambda
 get_ocaml 4.10.1-flambda
 get_ocaml 4.11.1-flambda
