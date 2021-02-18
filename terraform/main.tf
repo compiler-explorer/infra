9da161d0d5be6c4e879cd2ef61fee3313d41db07
Austin Morton <austinpmorton@gmail.com>
2021-02-17 23:25:59 -0500

    terraform version update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,15 @@
 provider "aws" {
   region  = "us-east-1"
-  version = "~> 3.7"
 }
 
 terraform {
   required_version = "~> 0.13"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws",
+      version = "~> 3.28.0"
+    }
+  }
   backend "s3" {
     bucket = "compiler-explorer"
     key    = "terraform/terraform.tfstate"
