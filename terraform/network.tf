8bc242754deeb91662c634df1a2ac5b326e696b3
Matt Godbolt <matt@godbolt.org>
2021-07-02 12:35:58 -0500

    Map public IPs on launch (required else somehow our nodes don't get IPs and that took the site down)

--- terraform/network.tf
+++ terraform/network.tf
@@ -31,9 +31,10 @@ resource "aws_default_route_table" "ce-route-table" {
 }
 
 resource "aws_subnet" "ce-1a" {
-  vpc_id            = aws_vpc.CompilerExplorer.id
-  cidr_block        = "172.30.0.0/24"
-  availability_zone = "us-east-1a"
+  vpc_id                  = aws_vpc.CompilerExplorer.id
+  cidr_block              = "172.30.0.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "CompilerExplorer1a"
@@ -41,9 +42,11 @@ resource "aws_subnet" "ce-1a" {
 }
 
 resource "aws_subnet" "ce-1b" {
-  vpc_id            = aws_vpc.CompilerExplorer.id
-  cidr_block        = "172.30.1.0/24"
-  availability_zone = "us-east-1b"
+  vpc_id                  = aws_vpc.CompilerExplorer.id
+  cidr_block              = "172.30.1.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
 
   tags = {
     Name = "CompilerExplorer1b"
@@ -51,9 +54,11 @@ resource "aws_subnet" "ce-1b" {
 }
 
 resource "aws_subnet" "ce-1c" {
-  vpc_id            = aws_vpc.CompilerExplorer.id
-  cidr_block        = "172.30.4.0/24"
-  availability_zone = "us-east-1c"
+  vpc_id                  = aws_vpc.CompilerExplorer.id
+  cidr_block              = "172.30.4.0/24"
+  availability_zone       = "us-east-1c"
+  map_public_ip_on_launch = true
+
 
   tags = {
     Name = "CompilerExplorer1c"
@@ -61,9 +66,10 @@ resource "aws_subnet" "ce-1c" {
 }
 
 resource "aws_subnet" "ce-1d" {
-  vpc_id            = aws_vpc.CompilerExplorer.id
-  cidr_block        = "172.30.2.0/24"
-  availability_zone = "us-east-1d"
+  vpc_id                  = aws_vpc.CompilerExplorer.id
+  cidr_block              = "172.30.2.0/24"
+  availability_zone       = "us-east-1d"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "CompilerExplorer1d"
@@ -71,9 +77,10 @@ resource "aws_subnet" "ce-1d" {
 }
 
 resource "aws_subnet" "ce-1e" {
-  vpc_id            = aws_vpc.CompilerExplorer.id
-  cidr_block        = "172.30.6.0/24"
-  availability_zone = "us-east-1e"
+  vpc_id                  = aws_vpc.CompilerExplorer.id
+  cidr_block              = "172.30.6.0/24"
+  availability_zone       = "us-east-1e"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "CompilerExplorer1e"
@@ -81,9 +88,10 @@ resource "aws_subnet" "ce-1e" {
 }
 
 resource "aws_subnet" "ce-1f" {
-  vpc_id            = aws_vpc.CompilerExplorer.id
-  cidr_block        = "172.30.5.0/24"
-  availability_zone = "us-east-1f"
+  vpc_id                  = aws_vpc.CompilerExplorer.id
+  cidr_block              = "172.30.5.0/24"
+  availability_zone       = "us-east-1f"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "CompilerExplorer1f"
