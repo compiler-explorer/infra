269f2ef4733fb720e19160c5a0d135f1d851d2c6
Matt Godbolt <matt@godbolt.org>
2021-07-11 12:25:42 -0500

    Hopefully fix tests

--- lambda/stats_test.py
+++ lambda/stats_test.py
@@ -3,6 +3,7 @@ import os
 from unittest import mock
 
 import botocore.session
+import pytest as pytest
 from aws_embedded_metrics.logger.metrics_logger import MetricsLogger
 from botocore.stub import Stubber, ANY
 
@@ -11,17 +12,26 @@ from stats import handle_sqs, handle_pageload
 SOME_DATE = datetime.datetime(2020, 1, 2, 3, 4, 5, 12312)
 
 
+@pytest.fixture
+def sqs_client():
+    return botocore.session.get_session().create_client('sqs', region_name='not-real')
+
+
+@pytest.fixture
+def s3_client():
+    return botocore.session.get_session().create_client('s3', region_name='not-real')
+
+
 def make_expected_body(msg_type: str, value: str):
     return f'{{"date": "2020-01-02", "time": "03:04:05.012312", "type": "{msg_type}", "value": "{value}"}}'
 
 
 @mock.patch.dict(os.environ, dict(S3_BUCKET_NAME="not-a-real-bucket"))
-def test_should_store_results_from_sqs_correctly():
+def test_should_store_results_from_sqs_correctly(s3_client):
     context = mock.Mock(function_name="some_func")
     event = dict(Records=[dict(body="first"), dict(body="second")])
-    s3 = botocore.session.get_session().create_client('s3')
 
-    with Stubber(s3) as stubber:
+    with Stubber(s3_client) as stubber:
         stubber.add_response(
             'put_object',
             {},
@@ -31,7 +41,7 @@ def test_should_store_results_from_sqs_correctly():
                 Key='stats/some_func-2020-01-02-03:04:05.012312.log'
             )
         )
-        handle_sqs(event, context, s3, SOME_DATE)
+        handle_sqs(event, context, s3_client, SOME_DATE)
 
 
 def test_pageloads_should_return_a_200_doc():
@@ -43,9 +53,8 @@ def test_pageloads_should_return_a_200_doc():
     metrics.put_metric.assert_called_once_with('PageLoad', 1)
 
 
-def test_should_handle_pageloads_with_no_sponsors():
+def test_should_handle_pageloads_with_no_sponsors(sqs_client):
     metrics = mock.Mock(spec_set=MetricsLogger)
-    sqs_client = botocore.session.get_session().create_client('sqs')
     queue_url = 'some-queue-url'
     with Stubber(sqs_client) as stubber:
         stubber.add_response(
@@ -61,9 +70,8 @@ def test_should_handle_pageloads_with_no_sponsors():
     metrics.put_metric.assert_called_once_with('PageLoad', 1)
 
 
-def test_should_handle_pageloads_with_empty_sponsors():
+def test_should_handle_pageloads_with_empty_sponsors(sqs_client):
     metrics = mock.Mock(spec_set=MetricsLogger)
-    sqs_client = botocore.session.get_session().create_client('sqs')
     queue_url = 'some-queue-url'
     with Stubber(sqs_client) as stubber:
         stubber.add_response(
@@ -75,9 +83,8 @@ def test_should_handle_pageloads_with_empty_sponsors():
     metrics.set_property.assert_called_once_with('sponsors', [])
 
 
-def test_should_handle_pageloads_with_one_sponsor():
+def test_should_handle_pageloads_with_one_sponsor(sqs_client):
     metrics = mock.Mock(spec_set=MetricsLogger)
-    sqs_client = botocore.session.get_session().create_client('sqs')
     queue_url = 'some-queue-url'
     with Stubber(sqs_client) as stubber:
         stubber.add_response('send_message', {}, dict(QueueUrl=queue_url, MessageBody=ANY))
@@ -90,9 +97,8 @@ def test_should_handle_pageloads_with_one_sponsor():
     metrics.set_property.assert_called_once_with('sponsors', ['bob'])
 
 
-def test_should_handle_pageloads_with_many_sponsors():
+def test_should_handle_pageloads_with_many_sponsors(sqs_client):
     metrics = mock.Mock(spec_set=MetricsLogger)
-    sqs_client = botocore.session.get_session().create_client('sqs')
     queue_url = 'some-queue-url'
     with Stubber(sqs_client) as stubber:
         stubber.add_response('send_message', {}, dict(QueueUrl=queue_url, MessageBody=ANY))
