6a5253a157253d181cd3633e973872bc9b79a991
Patrick Quist <partouf@gmail.com>
2021-07-28 16:51:08 +0200

    change ephemeral device point

--- setup-builder-startup.sh
+++ setup-builder-startup.sh
@@ -8,23 +8,23 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 systemctl stop docker
 umount /ephemeral || true
-sfdisk -f /dev/nvme1n1 <<EOF
+sfdisk -f /dev/nvme0n1 <<EOF
 label: dos
 label-id: 0x7d7e658b
-device: /dev/nvme1n1
+device: /dev/nvme0n1
 unit: sectors
 
-/dev/nvme1n1p1 : start=     3999744, size=   120999936, type=82
-/dev/nvme1n1p2 : start=   124999680, size=   656248832, type=83
+/dev/nvme0n1p1 : start=     3999744, size=   120999936, type=82
+/dev/nvme0n1p2 : start=   124999680, size=   656248832, type=83
 EOF
 sync
 sleep 2 # let the device get registered
-mkswap /dev/nvme1n1p1
-swapon /dev/nvme1n1p1
-mkfs.ext4 -F /dev/nvme1n1p2
+mkswap /dev/nvme0n1p1
+swapon /dev/nvme0n1p1
+mkfs.ext4 -F /dev/nvme0n1p2
 rm -rf /ephemeral
 mkdir /ephemeral
-mount /dev/nvme1n1p2 /ephemeral
+mount /dev/nvme0n1p2 /ephemeral
 
 cat >/etc/docker/daemon.json <<EOF
 {
