8fc1b70a7914e113e24455107ee9911228d4a0e8
Matt Godbolt <matt@godbolt.org>
2021-08-09 23:33:42 -0500

    Early rate limiting by IP attempts

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -144,6 +144,7 @@ resource "aws_cloudfront_distribution" "ce-godbolt-org" {
     error_caching_min_ttl = 5
     response_page_path    = "/admin/503.html"
   }
+  web_acl_id = aws_wafv2_web_acl.rate_limit.arn
 }
 
 # TODO - the duplication is rubbish
@@ -283,6 +284,7 @@ resource "aws_cloudfront_distribution" "compiler-explorer-com" {
     error_caching_min_ttl = 5
     response_page_path    = "/admin/503.html"
   }
+  web_acl_id = aws_wafv2_web_acl.rate_limit.arn
 }
 
 resource "aws_cloudfront_distribution" "godbo-lt" {
@@ -420,6 +422,8 @@ resource "aws_cloudfront_distribution" "godbo-lt" {
     error_caching_min_ttl = 5
     response_page_path    = "/admin/503.html"
   }
+
+  web_acl_id = aws_wafv2_web_acl.rate_limit.arn
 }
 
 resource "aws_cloudfront_distribution" "static-ce-cdn-net" {
@@ -477,13 +481,13 @@ resource "aws_cloudfront_distribution" "static-ce-cdn-net" {
       cookies {
         forward = "none"
       }
-      headers = [
+      headers                 = [
         # see https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/header-caching.html#header-caching-web-cors
         "Origin",
         "Access-Control-Request-Headers",
         "Access-Control-Request-Method"
       ]
-      query_string = true
+      query_string            = true
       query_string_cache_keys = [
         "v"
       ]
@@ -491,5 +495,52 @@ resource "aws_cloudfront_distribution" "static-ce-cdn-net" {
     target_origin_id       = "S3-ce-cdn.net"
     viewer_protocol_policy = "redirect-to-https"
     compress               = true
+  }
+}
+
+resource "aws_wafv2_web_acl" "rate_limit" {
+  name  = "RateLimitCompilerExplorer"
+  scope = "CLOUDFRONT"
+  default_action {
+    allow {}
+  }
+
+  rule {
+    name     = "RateLimitPost"
+    priority = 1
+    action {
+      //      block {}
+      count {}
+    }
+    statement {
+      rate_based_statement {
+        // 150 POSTs in 5 minutes - that's one every two seconds
+        limit              = 150
+        aggregate_key_type = "IP"
+        scope_down_statement {
+          byte_match_statement {
+            positional_constraint = "EXACTLY"
+            search_string         = "POST"
+            field_to_match {
+              method {}
+            }
+            text_transformation {
+              priority = 0
+              type     = "NONE"
+            }
+          }
+        }
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "ce_rate_limited_blocked"
+      sampled_requests_enabled   = true
+    }
+  }
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "ce_rate_limited_ok"
+    sampled_requests_enabled   = true
   }
 }
