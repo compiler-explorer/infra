05990169d005ac8e9658f9f50622cddd4bd6a6f7
Matt Godbolt <matt@godbolt.org>
2021-08-22 15:54:31 -0500

    Modules for ce_dns

---
+++ terraform/modules/ce_dns/outputs.tf
@@ -0,0 +1,3 @@
+output "zone_id" {
+  value = aws_route53_zone.zone.zone_id
+}
--- terraform/route53.tf
+++ terraform/route53.tf
@@ -42,153 +42,35 @@ resource "aws_route53_record" "static-ce-cdn-net-acm" {
   ttl     = 60
 }
 
-////////////////////////////////////////////
-
-resource "aws_route53_zone" "godbo-lt" {
-  name = "godbo.lt"
+module godbo-lt {
+  source = "./modules/ce_dns"
+  domain_name = "godbo.lt"
+  cloudfront_distribution = aws_cloudfront_distribution.godbo-lt
+  certificate = aws_acm_certificate.godbolt-org-et-al
 }
 
-resource "aws_route53_record" "godbo-lt" {
-  for_each = {
-    a    = "A"
-    aaaa = "AAAA"
-  }
-  zone_id  = aws_route53_zone.godbo-lt.zone_id
-  name     = ""
-  type     = each.value
-  alias {
-    name                   = aws_cloudfront_distribution.godbo-lt.domain_name
-    zone_id                = aws_cloudfront_distribution.godbo-lt.hosted_zone_id
-    evaluate_target_health = false
-  }
-}
-
-resource "aws_route53_record" "star-godbo-lt" {
-  zone_id = aws_route53_zone.godbo-lt.zone_id
-  name    = "*"
-  type    = "CNAME"
-  ttl     = 60
-  records = [aws_route53_zone.godbo-lt.name]
-}
-
-resource "aws_route53_record" "spf-godbo-lt" {
-  zone_id = aws_route53_zone.godbo-lt.zone_id
-  name    = ""
-  type    = "SPF"
-  ttl     = 3600
-  records = ["v=spf1 a include:_spf.google.com ~all"]
-}
-
-resource "aws_route53_record" "mail-godbo-lt" {
-  name    = ""
-  type    = "MX"
-  ttl     = 3600
-  zone_id = aws_route53_zone.godbo-lt.zone_id
-  records = [
-    "1 aspmx.l.google.com",
-    "5 alt1.aspmx.l.google.com",
-    "5 alt2.aspmx.l.google.com",
-    "10 alt3.aspmx.l.google.com",
-    "10 alt4.aspmx.l.google.com",
-  ]
-}
-
-resource "aws_route53_record" "godbo-lt-acm" {
-  for_each = {
-  for dvo in aws_acm_certificate.godbolt-org-et-al.domain_validation_options : dvo.domain_name => {
-    name   = dvo.resource_record_name
-    record = dvo.resource_record_value
-    type   = dvo.resource_record_type
-  }
-  }
-
-  allow_overwrite = true
-  name            = each.value.name
-  zone_id         = aws_route53_zone.godbo-lt.zone_id
-  type            = each.value.type
-  records         = [each.value.record]
-  ttl             = 60
-}
-
-////////////////////////////////
-
-resource "aws_route53_zone" "compiler-explorer-com" {
-  name = "compiler-explorer.com"
-}
-
-resource "aws_route53_record" "compiler-explorer-com" {
-  for_each = {
-    a    = "A"
-    aaaa = "AAAA"
-  }
-  zone_id  = aws_route53_zone.compiler-explorer-com.zone_id
-  name     = ""
-  type     = each.value
-  alias {
-    name                   = aws_cloudfront_distribution.compiler-explorer-com.domain_name
-    zone_id                = aws_cloudfront_distribution.compiler-explorer-com.hosted_zone_id
-    evaluate_target_health = false
-  }
-}
-
-resource "aws_route53_record" "star-compiler-explorer-com" {
-  zone_id = aws_route53_zone.compiler-explorer-com.zone_id
-  name    = "*"
-  type    = "CNAME"
-  ttl     = 60
-  records = [aws_route53_zone.compiler-explorer-com.name]
-}
-
-resource "aws_route53_record" "spf-compiler-explorer-com" {
-  zone_id = aws_route53_zone.compiler-explorer-com.zone_id
-  name    = ""
-  type    = "SPF"
-  ttl     = 3600
-  records = ["v=spf1 a include:_spf.google.com ~all"]
-}
-
-resource "aws_route53_record" "mail-compiler-explorer-com" {
-  name    = ""
-  type    = "MX"
-  ttl     = 3600
-  zone_id = aws_route53_zone.compiler-explorer-com.zone_id
-  records = [
-    "1 aspmx.l.google.com",
-    "5 alt1.aspmx.l.google.com",
-    "5 alt2.aspmx.l.google.com",
-    "10 alt3.aspmx.l.google.com",
-    "10 alt4.aspmx.l.google.com",
-  ]
-}
-
-resource "aws_route53_record" "compiler-explorer-com-acm" {
-  for_each = {
-  for dvo in aws_acm_certificate.godbolt-org-et-al.domain_validation_options : dvo.domain_name => {
-    name   = dvo.resource_record_name
-    record = dvo.resource_record_value
-    type   = dvo.resource_record_type
-  }
-  }
-
-  allow_overwrite = true
-  name            = each.value.name
-  zone_id         = aws_route53_zone.compiler-explorer-com.zone_id
-  type            = each.value.type
-  records         = [each.value.record]
-  ttl             = 60
+module compiler-explorer-com {
+  source = "./modules/ce_dns"
+  domain_name = "compiler-explorer.com"
+  cloudfront_distribution = aws_cloudfront_distribution.compiler-explorer-com
+  certificate = aws_acm_certificate.godbolt-org-et-al
 }
 
 resource "aws_route53_record" "gh-compiler-explorer-com" {
   name    = "_github-challenge-compiler-explorer"
-  zone_id = aws_route53_zone.compiler-explorer-com.zone_id
+  zone_id = module.compiler-explorer-com.zone_id
   ttl     = 3600
   type    = "TXT"
   records = ["a5417612c3"]
 }
 
+resource "aws_ses_domain_identity" "compiler-explorer-com" {
+  domain = "compiler-explorer.com"
+}
+
 resource "aws_route53_record" "ses-compiler-explorer-com" {
   name    = "_amazonses"
-  zone_id = aws_route53_zone.compiler-explorer-com.zone_id
+  zone_id = module.compiler-explorer-com.zone_id
   ttl     = 3600
   type    = "TXT"
   records = [aws_ses_domain_identity.compiler-explorer-com.verification_token]
