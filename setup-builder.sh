6c4585a2bfce4d32e6a296c8890ce425b5dd3477
Matt Godbolt <matt@godbolt.org>
2021-08-29 12:18:49 -0500

    More WIP

--- setup-builder.sh
+++ setup-builder.sh
@@ -11,7 +11,7 @@ wget -qO- https://get.docker.com/ | sh
 apt -y install python2.7 mosh fish jq ssmtp cronic subversion upx gdb
 chsh ubuntu -s /usr/bin/fish
 
-cd /home/ubuntu/infra
+cd /infra
 pip install --upgrade -r requirements.txt
 
 # Install private and public keys
@@ -20,7 +20,6 @@ aws ssm get-parameter --name /admin/ce_private_key | jq -r .Parameter.Value >/ho
 chmod 600 /home/ubuntu/.ssh/id_rsa
 aws s3 cp s3://compiler-explorer/authorized_keys/admin.key /home/ubuntu/.ssh/id_rsa.pub
 chown -R ubuntu:ubuntu /home/ubuntu/.ssh
-chown -R ubuntu:ubuntu /home/ubuntu/infra
 
 sudo -u ubuntu fish setup.fish
 crontab -u ubuntu crontab.builder
@@ -28,3 +27,6 @@ crontab -u ubuntu crontab.builder
 echo builder >/etc/hostname
 hostname builder
 sed -i "/127.0.0.1/c 127.0.0.1 localhost builder" /etc/hosts
+
+mv /infra /home/ubuntu/infra
+chown -R ubuntu:ubuntu /home/ubuntu/infra
