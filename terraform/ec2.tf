9ba1bbbd4c9cc11f09f4ddbd26ee6ddc4b4e8120
Patrick Quist <partouf@gmail.com>
2021-10-13 23:34:44 +0200

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -102,7 +102,7 @@ resource "aws_instance" "CERunner" {
   ami                         = local.prod_image_id
   iam_instance_profile        = aws_iam_instance_profile.Builder.name
   ebs_optimized               = true
-  instance_type               = "t2.small"
+  instance_type               = "t2.micro"
   monitoring                  = false
   key_name                    = "mattgodbolt"
   subnet_id                   = local.admin_subnet
