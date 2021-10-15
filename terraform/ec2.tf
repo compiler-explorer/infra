67ebcae8555561cb695c49eb1e2e3436adf9a692
Patrick Quist <partouf@gmail.com>
2021-10-15 21:24:38 +0200

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,5 +1,5 @@
 locals {
-  runner_image_id  = "ami-06971202708e1639a"
+  runner_image_id  = "ami-0cc27013e4e36f493"
   conan_image_id   = "ami-0b41dc7a318b530bd"
   builder_image_id = "ami-0ef4921e9d82c03fb"
   admin_subnet = module.ce_network.subnet["1a"].id
