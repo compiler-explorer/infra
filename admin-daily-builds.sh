935703a110afba01ce9f2af06efb48e852cb332e
Matt Godbolt <matt@godbolt.org>
2021-10-29 11:56:06 -0500

    Try some GH action magic

--- admin-daily-builds.sh
+++ admin-daily-builds.sh
@@ -113,11 +113,11 @@ build_libraries() {
 #MOVED: build_latest clang llvm build.sh llvm-trunk
 
 #MOVED: build_latest gcc gcc build.sh trunk
-build_latest gcc gcc_contracts build.sh lock3-contracts-trunk
-build_latest gcc gcc_contract_labels build.sh lock3-contract-labels-trunk
-build_latest gcc gcc_modules build.sh cxx-modules-trunk
-build_latest gcc gcc_coroutines build.sh cxx-coroutines-trunk
-build_latest gcc gcc_gccrs_master build.sh gccrs-master
+#MOVED: build_latest gcc gcc_contracts build.sh lock3-contracts-trunk
+#MOVED: build_latest gcc gcc_contract_labels build.sh lock3-contract-labels-trunk
+#MOVED: build_latest gcc gcc_modules build.sh cxx-modules-trunk
+#MOVED: build_latest gcc gcc_coroutines build.sh cxx-coroutines-trunk
+#MOVED: build_latest gcc gcc_gccrs_master build.sh gccrs-master
 #MOVED: build_latest clang clang build.sh trunk
 build_latest clang clang_assertions build.sh assertions-trunk
 build_latest clang clang_cppx build.sh cppx-trunk
