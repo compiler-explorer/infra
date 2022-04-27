6e05a23be940779328859cbc0bbb555d36afebfb
Matt Godbolt <matt@godbolt.org>
2022-04-27 17:18:55 -0500

    New image with no ts

--- terraform/lc.tf
+++ terraform/lc.tf
@@ -1,7 +1,7 @@
 locals {
-  image_id          = "ami-0558fd62add69b298"
-  staging_image_id  = "ami-0558fd62add69b298"
-  beta_image_id     = "ami-0558fd62add69b298"
+  image_id          = "ami-0b108983c68405d03"
+  staging_image_id  = "ami-0b108983c68405d03"
+  beta_image_id     = "ami-0b108983c68405d03"
   staging_user_data = base64encode("staging")
   beta_user_data    = base64encode("beta")
   // Current c5 on-demand price is 0.085. Yearly pre-pay is 0.05 (so this is same as prepaying a year)
