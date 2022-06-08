8448545b54655d8ee9195cf7b773f52f31f585c3
partouf <partouf@gmail.com>
2022-06-08 23:59:25 +0200

    add reformat command

---
+++ bin/lib/library_yaml.py
@@ -0,0 +1,23 @@
+import os
+import yaml
+from pathlib import Path
+
+from lib.config_safe_loader import ConfigSafeLoader
+
+class LibraryYaml:
+    def __init__(self, yaml_dir):
+        self.yaml_dir = yaml_dir
+        self.yaml_path = Path(os.path.join(self.yaml_dir, 'libraries.yaml'))
+        self.Load()
+
+    def Load(self):
+        with self.yaml_path.open() as yaml_file:
+            self.yaml_doc = yaml.load(yaml_file, Loader=ConfigSafeLoader)
+
+    def Save(self):
+       with open(self.yaml_path, "w") as yaml_file:
+           yaml.dump(self.yaml_doc, yaml_file)
+
+    def Reformat(self):
+        self.Load()
+        self.Save()
