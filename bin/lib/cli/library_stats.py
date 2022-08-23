fd9fd60027fbcd8455adf7ef318c3fcec86b8b23
partouf <partouf@gmail.com>
2022-08-23 20:58:44 +0200

    more snakes

--- bin/lib/cli/library_stats.py
+++ bin/lib/cli/library_stats.py
@@ -15,14 +15,14 @@ def library_stats_update():
     client = boto3.client('athena')
 
     fields = 'count(*) as times_used, min(time) as first_used, max(time) as last_used, split_part(request_url, \'/\', 6) as library, split_part(request_url, \'/\', 7) as library_version'
-    serverurl = 'conan.compiler-explorer.com'
-    requesturl = 'https://conan.compiler-explorer.com:1443/v1/conans/%/download_urls'
-    where = f'WHERE domain_name=\'{serverurl}\' and request_url like \'{requesturl}\''
-    groupby = 'GROUP BY split_part(request_url, \'/\', 6), split_part(request_url, \'/\', 7)'
-    orderby = 'ORDER BY count(*) DESC'
+    server_url = 'conan.compiler-explorer.com'
+    request_url = 'https://conan.compiler-explorer.com:1443/v1/conans/%/download_urls'
+    where = f'WHERE domain_name=\'{server_url}\' and request_url like \'{request_url}\''
+    group_by = 'GROUP BY split_part(request_url, \'/\', 6), split_part(request_url, \'/\', 7)'
+    order_by = 'ORDER BY count(*) DESC'
 
     response = client.start_query_execution(
-        QueryString=f'SELECT {fields} FROM alb_logs {where} {groupby} {orderby};',
+        QueryString=f'SELECT {fields} FROM alb_logs {where} {group_by} {order_by};',
         QueryExecutionContext={
             'Database': 'default'
         },
@@ -32,23 +32,23 @@ def library_stats_update():
         WorkGroup='primary'
     )
 
-    queryExecutionId = response['QueryExecutionId']
+    query_execution_id = response['QueryExecutionId']
 
     status = 'QUEUED'
     while status in ['QUEUED', 'RUNNING']:
         response = client.get_query_execution(
-            QueryExecutionId=queryExecutionId)
+            QueryExecutionId=query_execution_id)
         status = response['QueryExecution']['Status']['State']
 
     if status == 'SUCCEEDED':
-        querycsvurl = f's3://compiler-explorer/public/{queryExecutionId}.csv'
-        querymetaurl = f's3://compiler-explorer/public/{queryExecutionId}.csv.metadata'
-        pubcsvurl = 's3://compiler-explorer/public/library_usage.csv'
+        query_csv_url = f's3://compiler-explorer/public/{query_execution_id}.csv'
+        query_meta_url = f's3://compiler-explorer/public/{query_execution_id}.csv.metadata'
+        library_usage_csv_url = 's3://compiler-explorer/public/library_usage.csv'
 
         os.system(
-            f'aws s3 cp --acl public-read "{querycsvurl}" "{pubcsvurl}"')
-        os.system(f'aws s3 rm "{querymetaurl}"')
-        os.system(f'aws s3 rm "{querycsvurl}"')
+            f'aws s3 cp --acl public-read "{query_csv_url}" "{library_usage_csv_url}"')
+        os.system(f'aws s3 rm "{query_meta_url}"')
+        os.system(f'aws s3 rm "{query_csv_url}"')
 
         print('Ok')
     else:
