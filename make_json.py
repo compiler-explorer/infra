00e98055b67dd3c268c4cbb0722c11c9525309ea
Matt Godbolt <matt@godbolt.org>
2022-08-26 09:10:27 -0500

    WIP

--- make_json.py
+++ make_json.py
@@ -4,7 +4,7 @@ import json
 
 obj = {}
 config = ConfigParser()
-config.read(os.path.join(os.getenv("HOME"), ".aws", "credentials"))
+config.read(os.path.join(os.getenv("HOME", ""), ".aws", "credentials"))
 obj["MY_ACCESS_KEY"] = config.get("default", "aws_access_key_id", fallback="")
 obj["MY_SECRET_KEY"] = config.get("default", "aws_secret_access_key", fallback="")
 
