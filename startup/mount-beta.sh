cb1a0cb2b5100a99b50362a3711dd20c50a79de1
partouf <partouf@gmail.com>
2022-09-09 21:46:07 +0200

    fix opt/ce

--- startup/mount-beta.sh
+++ startup/mount-beta.sh
@@ -3,8 +3,7 @@ mount_opt() {
     local cefsroot=$1
 
     unlink /opt/compiler-explorer
-    unlink /root/ce
-    ln -sfT "/cefs/${cefsroot}" /root/ce
+    ln -sfT "/cefs/${cefsroot}" /opt/compiler-explorer
 
     mkdir -p /opt/intel
     mountpoint /opt/intel || mount --bind /efs/intel /opt/intel
