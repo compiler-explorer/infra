6ddaf144b1ec7df595e004691b90842de2c4c7f2
Matt Godbolt <matt@godbolt.org>
2022-09-11 10:46:48 -0500

    Last of the shellcheckæ

--- init/start-conan.sh
+++ init/start-conan.sh
@@ -11,11 +11,14 @@ git pull
 npm i -g npm
 npm i
 
-export CESECRET=$(aws ssm get-parameter --name /compiler-explorer/conanproxysecret | jq -r .Parameter.Value)
-export CEPASSWORD=$(aws ssm get-parameter --name /compiler-explorer/conanpwd | jq -r .Parameter.Value)
+CESECRET=$(aws ssm get-parameter --name /compiler-explorer/conanproxysecret | jq -r .Parameter.Value)
+CEPASSWORD=$(aws ssm get-parameter --name /compiler-explorer/conanpwd | jq -r .Parameter.Value)
+export CESECRET, CEPASSWORD
 
 sudo -u ce -H /home/${CE_USER}/.local/bin/gunicorn -b 0.0.0.0:9300 -w 4 -t 300 conans.server.server_launcher:app &
 
+# shellcheck disable=SC2086
+#  (we deliberately pass multiple args in ${EXTRA_ARGS}
 exec sudo -u ce -H --preserve-env=NODE_ENV,PATH,CE_USER,CESECRET,CEPASSWORD -- \
     /home/ubuntu/node/bin/node \
     -- index.js \
--- remove_old_compilers.sh
+++ remove_old_compilers.sh
@@ -6,16 +6,16 @@ S3=s3://compiler-explorer/opt/
 remove_older() {
     local compiler=$1
     for old_file in $(aws s3 ls ${S3} | grep -oE "\s${compiler}"'-trunk-[0-9]+.*' | sort | head -n -${KEEP_LAST}); do
-        echo Removing ${S3}${old_file}
-        aws s3 rm ${S3}${old_file}
+        echo Removing "${S3}${old_file}"
+        aws s3 rm "${S3}${old_file}"
     done
 }
 
 remove_older_master() {
     local compiler=$1
     for old_file in $(aws s3 ls ${S3} | grep -oE "\s${compiler}"'-master-[0-9]+.*' | sort | head -n -${KEEP_LAST}); do
-        echo Removing ${S3}${old_file}
-        aws s3 rm ${S3}${old_file}
+        echo Removing "${S3}${old_file}"
+        aws s3 rm "${S3}${old_file}"
     done
 }
 
