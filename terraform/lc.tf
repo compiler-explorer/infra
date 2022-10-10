fb063e3c5e90282b37b044a5d127623d6ce73c40
Matt Godbolt <matt@godbolt.org>
2022-10-10 08:20:51 -0500

    Bump to latest image (new nsolid/node too)

--- terraform/lc.tf
+++ terraform/lc.tf
@@ -1,7 +1,7 @@
 locals {
-  image_id          = "ami-0cc6fd5f52bd05b88"
-  staging_image_id  = "ami-0cc6fd5f52bd05b88"
-  beta_image_id     = "ami-0cc6fd5f52bd05b88"
+  image_id          = "ami-059df80d2d990dc65"
+  staging_image_id  = "ami-059df80d2d990dc65"
+  beta_image_id     = "ami-059df80d2d990dc65"
   staging_user_data = base64encode("staging")
   beta_user_data    = base64encode("beta")
   // Current c5 on-demand price is 0.085. Yearly pre-pay is 0.05 (so this is same as prepaying a year)
