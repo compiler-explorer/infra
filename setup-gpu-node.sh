f19d651a0cd4065e4a55e5043d374c8ae25ee305
partouf <partouf@gmail.com>
2022-11-05 00:43:09 +0100

    install nvidia driver

--- setup-gpu-node.sh
+++ setup-gpu-node.sh
@@ -100,5 +100,10 @@ adduser --system --group ce
 
 # setup nvidia drivers https://docs.nvidia.com/cuda/cuda-installation-guide-linux/index.html#runfile-nouveau-ubuntu
 
+pushd /tmp
+wget https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda_11.8.0_520.61.05_linux.run
+sh cuda_11.8.0_520.61.05_linux.run --silent --driver
+popd
+
 echo -e "blacklist nouveau\noptions nouveau modeset=0\n" > /etc/modprobe.d/blacklist-nouveau.conf
 update-initramfs -u
