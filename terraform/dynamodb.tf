e99cd72c19ad57c4a70269bad51389708ef32d3a
partouf <partouf@gmail.com>
2022-11-05 13:51:27 +0100

    new dbtable

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -27,6 +27,35 @@ resource "aws_dynamodb_table" "links" {
   }
 }
 
+resource "aws_dynamodb_table" "gpulinks" {
+  name = "gpulinks"
+  lifecycle {
+    ignore_changes = [
+      read_capacity,
+      write_capacity
+    ]
+    prevent_destroy = true
+  }
+  billing_mode   = "PAY_PER_REQUEST"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "prefix"
+  range_key      = "unique_subhash"
+
+  attribute {
+    name = "prefix"
+    type = "S"
+  }
+  attribute {
+    name = "unique_subhash"
+    type = "S"
+  }
+
+  point_in_time_recovery {
+    enabled = true
+  }
+}
+
 resource "aws_dynamodb_table" "versionslog" {
   name = "versionslog"
   lifecycle {
