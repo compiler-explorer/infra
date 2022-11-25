23808538c27b09b009c5a4f49591f97f3aac3f67
Matt Godbolt <matt@godbolt.org>
2022-11-25 12:08:12 -0600

    More tweaks to ci setup

--- setup-ci.sh
+++ setup-ci.sh
@@ -8,3 +8,7 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "${DIR}"
 
 env EXTRA_NFS_ARGS="" "${DIR}/setup-common.sh" ci
+
+ln -s /efs/squash-images /opt/squash-images
+ln -s /efs/compiler-explorer /opt/compiler-explorer
+ln -s /efs/wine-stable /opt/wine-stable
