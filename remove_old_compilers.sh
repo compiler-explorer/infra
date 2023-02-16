3b1bc71e777dfa97d79a5e4eedebf1d19bdd2ea2
Matt Godbolt <matt@godbolt.org>
2023-02-15 20:58:42 -0600

    Tweaks

--- remove_old_compilers.sh
+++ remove_old_compilers.sh
@@ -3,10 +3,11 @@
 KEEP_LAST=5
 S3=s3://compiler-explorer/opt/
 
-for compiler_prefix in $(aws s3 ls ${S3} | grep -oE '[-a-zA-Z0-9_]+-(trunk|master)-' | sort -u); do
+for compiler_prefix in $(aws s3 ls ${S3} | grep -oE '[-a-zA-Z0-9_]+-(main|trunk|master)-' | sort -u); do
   echo "Considering prefix ${compiler_prefix}"
   aws s3 ls "${S3}${compiler_prefix}"
-  for old_file in $(aws s3 ls "${S3}${compiler_prefix}" | awk '{print $4}' | sort | head -n -${KEEP_LAST}); do
+  # Look for anything with a datelike bit going on for extra safety.
+  for old_file in $(aws s3 ls "${S3}${compiler_prefix}" | grep -E '[0-9]{8}\.tar'| awk '{print $4}' | sort | head -n -${KEEP_LAST}); do
     echo Removing "${S3}${old_file}"
     aws s3 rm "${S3}${old_file}"
   done
