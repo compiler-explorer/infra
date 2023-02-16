8e21353a0d3c1a9e7476f731116ee40978a5cfa2
Matt Godbolt <matt@godbolt.org>
2023-02-15 20:51:01 -0600

    Delete all old compilers. Heinous bash, will come to haunt me I swear. Closes #943

--- remove_old_compilers.sh
+++ remove_old_compilers.sh
@@ -3,56 +3,11 @@
 KEEP_LAST=5
 S3=s3://compiler-explorer/opt/
 
-remove_older() {
-    local compiler=$1
-    for old_file in $(aws s3 ls ${S3} | grep -oE "\s${compiler}"'-trunk-[0-9]+.*' | sort | head -n -${KEEP_LAST}); do
-        echo Removing "${S3}${old_file}"
-        aws s3 rm "${S3}${old_file}"
-    done
-}
-
-remove_older_master() {
-    local compiler=$1
-    for old_file in $(aws s3 ls ${S3} | grep -oE "\s${compiler}"'-master-[0-9]+.*' | sort | head -n -${KEEP_LAST}); do
-        echo Removing "${S3}${old_file}"
-        aws s3 rm "${S3}${old_file}"
-    done
-}
-
-remove_older llvm
-remove_older gcc
-remove_older gcc-lock3-contracts
-remove_older gcc-lock3-contract-labels
-remove_older gcc-cxx-modules
-remove_older gcc-cxx-coroutines
-remove_older gcc-embed
-remove_older gcc-static-analysis
-remove_older_master gcc-gccrs
-remove_older clang
-remove_older clang-assertions
-remove_older clang-autonsdmi
-remove_older clang-cppx
-remove_older clang-cppx-ext
-remove_older clang-cppx-p2320
-remove_older clang-dang-main
-remove_older clang-embed
-remove_older clang-lifetime
-remove_older clang-llvmflang
-remove_older clang-parmexpr
-remove_older clang-patmat
-remove_older clang-relocatable
-remove_older clang-rocm-trunk
-remove_older clang-widberg-main
-remove_older llvm-spirv
-remove_older go
-remove_older tinycc
-remove_older cc65
-remove_older_master mrustc
-remove_older_master cproc
-remove_older_master rustc-cg-gcc
-remove_older_master SPIRV-Tools
-remove_older arm-gcc
-remove_older arm64-gcc
-remove_older ispc
-remove_older mrisc32
-remove_older tendra
+for compiler_prefix in $(aws s3 ls ${S3} | grep -oE '[-a-zA-Z0-9_]+-(trunk|master)-' | sort -u); do
+  echo "Considering prefix ${compiler_prefix}"
+  aws s3 ls "${S3}${compiler_prefix}"
+  for old_file in $(aws s3 ls "${S3}${compiler_prefix}" | awk '{print $4}' | sort | head -n -${KEEP_LAST}); do
+    echo Removing "${S3}${old_file}"
+    aws s3 rm "${S3}${old_file}"
+  done
+done
