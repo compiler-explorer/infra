23e74b4d78153b359701502645b10316027aeaa5
partouf <partouf@gmail.com>
2023-03-08 11:32:54 +0100

    propdebug

--- init/run.ps1
+++ init/run.ps1
@@ -26,7 +26,7 @@ MountZ
 $env:NODE_ENV = "production"
 $env:PATH = "$env:PATH;Z:/compilers/mingw-8.1.0/mingw64/bin"
 
-$nodeargs = ("--max_old_space_size=6000","--","app.js","--debug","--dist","--port","10240","--metricsPort","10241","--suppressConsoleLog","--logHost",$LogHost,"--logPort",$LogPort,"--env","amazonwin","--env",$CeEnv,"--language","c++,pascal")
+$nodeargs = ("--max_old_space_size=6000","--","app.js","--debug","--dist","--port","10240","--metricsPort","10241","--suppressConsoleLog","--logHost",$LogHost,"--logPort",$LogPort,"--env","amazonwin","--env",$CeEnv,"--language","c++","--propDebug","true")
 
 Set-Location -Path "C:\compilerexplorer"
 #Set-Location -Path "D:\git\compiler-explorer"
