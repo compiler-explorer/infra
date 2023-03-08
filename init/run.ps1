d0e6782700f2e0f3cfadf47b307deaad0a0c81a1
partouf <partouf@gmail.com>
2023-03-08 12:43:48 +0100

    no need to log to file, already done by nssm

--- init/run.ps1
+++ init/run.ps1
@@ -29,7 +29,5 @@ $env:PATH = "$env:PATH;Z:/compilers/mingw-8.1.0/mingw64/bin"
 $nodeargs = ("--max_old_space_size=6000","--","app.js","--debug","--dist","--port","10240","--metricsPort","10241","--suppressConsoleLog","--logHost",$LogHost,"--logPort",$LogPort,"--env","amazonwin","--env",$CeEnv,"--language","c++","--propDebug","true")
 
 Set-Location -Path "C:\compilerexplorer"
-#Set-Location -Path "D:\git\compiler-explorer"
 
-& 'C:\Program Files\nodejs\node.exe' $nodeargs >> /tmp/node-log.txt
-#& 'D:\Program Files\nodejs\node.exe' $nodeargs
+& 'C:\Program Files\nodejs\node.exe' $nodeargs
