e7e28fde94c3be1e82478868605767fab9499e0a
partouf <partouf@gmail.com>
2023-03-08 14:03:34 +0100

    disable pprtrail for a bit

--- init/run.ps1
+++ init/run.ps1
@@ -26,7 +26,8 @@ MountZ
 $env:NODE_ENV = "production"
 $env:PATH = "$env:PATH;Z:/compilers/mingw-8.1.0/mingw64/bin"
 
-$nodeargs = ("--max_old_space_size=6000","--","app.js","--debug","--dist","--port","10240","--metricsPort","10241","--suppressConsoleLog","--logHost",$LogHost,"--logPort",$LogPort,"--env","amazonwin","--env",$CeEnv,"--language","c++","--propDebug","true")
+# $nodeargs = ("--max_old_space_size=6000","--","app.js","--debug","--dist","--port","10240","--metricsPort","10241","--suppressConsoleLog","--logHost",$LogHost,"--logPort",$LogPort,"--env","amazonwin","--env",$CeEnv,"--language","c++","--propDebug","true")
+$nodeargs = ("--max_old_space_size=6000","--","app.js","--debug","--dist","--port","10240","--metricsPort","10241","--env","amazonwin","--env",$CeEnv,"--language","c++","--propDebug","true")
 
 Set-Location -Path "C:\compilerexplorer"
 
