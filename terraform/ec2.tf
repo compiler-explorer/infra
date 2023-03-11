0b27345f977bea00953b90b2d897b5ba4f2b02cd
partouf <partouf@gmail.com>
2023-03-12 00:13:13 +0100

    change smbserver instance type

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -135,7 +135,7 @@ resource "aws_instance" "CESMBServer" {
   ami                         = local.smbserver_image_id
   iam_instance_profile        = aws_iam_instance_profile.CompilerExplorerRole.name
   ebs_optimized               = false
-  instance_type               = "t2.micro"
+  instance_type               = "t3.micro"
   monitoring                  = false
   key_name                    = "mattgodbolt"
   subnet_id                   = local.admin_subnet
