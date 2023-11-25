218d464c8d7aa9a34bba1c165da5a70475d2ae53
Matt Godbolt <matt@godbolt.org>
2023-11-25 13:07:52 -0600

    Reinstate hard link

--- setup-ci.sh
+++ setup-ci.sh
@@ -10,5 +10,10 @@ cd "${DIR}"
 env EXTRA_NFS_ARGS="" "${DIR}/setup-common.sh" ci
 
 ln -s /efs/squash-images /opt/squash-images
-ln -s /efs/compiler-explorer /opt/compiler-explorer
 ln -s /efs/wine-stable /opt/wine-stable
+# Some things try to canonicalise their installation. Ideally nothing
+# would end up building with references to `/opt/compiler-explorer`
+# hardcoded in to it, but we have some compilers that do. And some of
+# those also the canonicalise the path and end up hardcoding `/efs/...`
+# which is the wrong path on the installed machines. So..a hard link here...
+ln -d /efs/compiler-explorer /opt/compiler-explorer
