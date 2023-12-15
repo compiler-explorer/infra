8ffa876f7f9a7a3428c281052251b517f5971438
Kevin Jeon <kevinjeon@google.com>
2023-12-15 18:48:14 -0500

    Add nightly dex2oat compiler
    This change adds dex2oat for Android as a nightly compiler. The zip file
    containing the compiler and other necessary artifacts is pulled from the
    latest AOSP build using the fetch_art_release_from_head.sh script.
    
    The script has been uploaded into the dex2oat/ directory, and script.py
    has been modified to make the 'fetch' config optional.

---
+++ bin/yaml/android-java/fetch_art_release_from_head.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Fetches ART binaries and ART bootclasspath jars from AOSP head.
+# Saves the result at `art_release.zip`.
+
+set -e
+
+URL=https://ci.android.com/builds/latest/branches/aosp-main/targets/aosp_arm64-trunk_staging-userdebug/view/BUILD_INFO
+RURL=$(curl -Ls -o /dev/null -w %{url_effective} "${URL}")
+wget "${RURL%/view/BUILD_INFO}/raw/art_release.zip"
