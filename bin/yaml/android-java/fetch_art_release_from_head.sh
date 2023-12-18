b690c6e23ba4a379d9876e6c2bc23ca8a2111ada
Kevin Jeon <kevinjeon@google.com>
2023-12-18 12:18:59 -0500

    Add quotes around curly braces
    Addresses SC1083 warning in the fetch_art_release_from_head.sh script.

--- bin/yaml/android-java/fetch_art_release_from_head.sh
+++ bin/yaml/android-java/fetch_art_release_from_head.sh
@@ -6,5 +6,5 @@
 set -e
 
 URL=https://ci.android.com/builds/latest/branches/aosp-main/targets/aosp_arm64-trunk_staging-userdebug/view/BUILD_INFO
-RURL=$(curl -Ls -o /dev/null -w %{url_effective} "${URL}")
+RURL=$(curl -Ls -o /dev/null -w "%{url_effective}" "${URL}")
 wget "${RURL%/view/BUILD_INFO}/raw/art_release.zip"
