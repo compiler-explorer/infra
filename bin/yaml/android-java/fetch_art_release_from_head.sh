c0d095674ce090a406d4149a39efc446d11b4481
Kevin Jeon <kevinjeon@google.com>
2023-12-19 11:20:52 -0500

    Modify dex2oat fetch script
    Updates bash flags and replaces wget with curl --location.

--- bin/yaml/android-java/fetch_art_release_from_head.sh
+++ bin/yaml/android-java/fetch_art_release_from_head.sh
@@ -3,8 +3,8 @@
 # Fetches ART binaries and ART bootclasspath jars from AOSP head.
 # Saves the result at `art_release.zip`.
 
-set -e
+set -euo pipefail
 
 URL=https://ci.android.com/builds/latest/branches/aosp-main/targets/aosp_arm64-trunk_staging-userdebug/view/BUILD_INFO
 RURL=$(curl -Ls -o /dev/null -w "%{url_effective}" "${URL}")
-wget "${RURL%/view/BUILD_INFO}/raw/art_release.zip"
+curl "${RURL%/view/BUILD_INFO}/raw/art_release.zip" --location --output art_release.zip
