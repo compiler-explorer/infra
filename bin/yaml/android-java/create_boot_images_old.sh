2d868fc979dd0ac57e164422479b2c699a4e269a
Kevin Jeon <kevinjeon@google.com>
2024-05-31 14:20:44 -0400

    Add dex2oat 33.10
    This is an older version of dex2oat that will be helpful for
    demonstrating some of the improvements made in version 34. A different
    script for creating boot images has been added, as CMC GC and riscv64
    aren't supported.

---
+++ bin/yaml/android-java/create_boot_images_old.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Older ART versions do not support CMC or riscv64. Otherwise, this script is
+# identical to the create_boot_images.sh script.
+
+set -euo pipefail
+
+DIR_FOR_BOOT_IMAGE=app/system/framework    # Using /app directory, which is used for creating oat output in the compilation commands
+declare -a instruction_sets=("arm" "arm64" "x86" "x86_64")
+
+for instruction_set in "${instruction_sets[@]}"
+do
+    mkdir -p $DIR_FOR_BOOT_IMAGE/"$instruction_set"
+x86_64/bin/dex2oat64 \
+    --runtime-arg -Xms64m \
+    --runtime-arg -Xmx512m \
+    --runtime-arg -Xbootclasspath:bootjars/core-oj.jar:bootjars/core-libart.jar:bootjars/okhttp.jar:bootjars/bouncycastle.jar:bootjars/apache-xml.jar \
+    --runtime-arg -Xbootclasspath-locations:/apex/com.android.art/javalib/core-oj.jar:/apex/com.android.art/javalib/core-libart.jar:/apex/com.android.art/javalib/okhttp.jar:/apex/com.android.art/javalib/bouncycastle.jar:/apex/com.android.art/javalib/apache-xml.jar \
+    --instruction-set="$instruction_set" \
+    --compiler-filter=speed \
+    --dex-file=bootjars/core-oj.jar \
+    --dex-file=bootjars/core-libart.jar \
+    --dex-file=bootjars/okhttp.jar \
+    --dex-file=bootjars/bouncycastle.jar \
+    --dex-file=bootjars/apache-xml.jar \
+    --dex-location=/apex/com.android.art/javalib/core-oj.jar \
+    --dex-location=/apex/com.android.art/javalib/core-libart.jar \
+    --dex-location=/apex/com.android.art/javalib/okhttp.jar \
+    --dex-location=/apex/com.android.art/javalib/bouncycastle.jar \
+    --dex-location=/apex/com.android.art/javalib/apache-xml.jar \
+    --image=$DIR_FOR_BOOT_IMAGE/"$instruction_set"/boot.art \
+    --oat-file=$DIR_FOR_BOOT_IMAGE/"$instruction_set"/boot.oat \
+    --output-vdex=$DIR_FOR_BOOT_IMAGE/"$instruction_set"/boot.vdex \
+    --android-root=out/empty \
+    --abort-on-hard-verifier-error \
+    --no-abort-on-soft-verifier-error \
+    --compilation-reason=cloud \
+    --image-format=lz4 \
+    --force-determinism \
+    --resolve-startup-const-strings=true \
+    --avoid-storing-invocation \
+    --generate-mini-debug-info \
+    --force-allow-oj-inlines \
+    --no-watch-dog \
+    --base=0x70000000
+done
