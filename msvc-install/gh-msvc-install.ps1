ae5f5c7364396581c45b97f4b2ed9b2e8e5c51a6
Patrick Quist <partouf@gmail.com>
2024-06-29 00:12:38 +0200

    Update gh-msvc-install.ps1

--- msvc-install/gh-msvc-install.ps1
+++ msvc-install/gh-msvc-install.ps1
@@ -29,7 +29,7 @@ function Install
 
     New-Item -ItemType Directory -Force "$full_install_root"
     Write-Host "Installating in : $full_install_root"
-    Start-Process -Wait -FilePath "$installer" -ArgumentList @("--quiet", "--installPath", "$full_install_root", "--add", "Microsoft.VisualStudio.Component.VC.Tools.x86.x64", "--add", "Microsoft.VisualStudio.Component.VC.Tools.ARM64", "--add", "Microsoft.VisualStudio.Component.VC.Tools.ARM")
+    Start-Process -Wait -FilePath "$installer" -ArgumentList @("--quiet", "--installPath", "$full_install_root", "--add", "Microsoft.VisualStudio.Component.VC.Tools.x86.x64", "--add", "Microsoft.VisualStudio.Component.VC.Tools.ARM64", "--add", "Microsoft.VisualStudio.Component.VC.Tools.ARM", "--add", "Microsoft.VisualStudio.Component.VC.ASAN")
 }
 
 function ZipAndUpload
