b227b71bd90e1cc41f030ea905b69e6439a7add2
Partouf <partouf@gmail.com>
2024-07-08 15:20:11 +0200

    fix params and names

--- queue-lambda/queue-sendmessage.js
+++ queue-lambda/queue-sendmessage.js
@@ -1,10 +1,10 @@
 import {ApiGatewayManagementApiClient, PostToConnectionCommand} from '@aws-sdk/client-apigatewaymanagementapi';
 import {QueueConnections} from './queue-connections.js';
 
-async function send_message(apiGwClient, id, postData) {
+async function send_message(apiGwClient, connectionId, postData) {
     try {
         const postToConnectionCommand = new PostToConnectionCommand({
-            ConnectionId: id,
+            ConnectionId: connectionId,
             Data: postData,
         });
         await apiGwClient.send(postToConnectionCommand);
@@ -12,7 +12,7 @@ async function send_message(apiGwClient, id, postData) {
         return true;
     } catch (e) {
         if (e.statusCode === 410) {
-            QueueConnections.remove(id);
+            QueueConnections.remove(connectionId);
             return false;
         } else {
             throw e;
@@ -20,29 +20,29 @@ async function send_message(apiGwClient, id, postData) {
     }
 }
 
-async function relay_request(arch, request) {
-    const subscribers = await QueueConnections.subscribers(arch);
-    if (subscribers.Items || subscribers.Items.length === 0) throw new Error('No listeners for ' + arch);
+async function relay_request(apiGwClient, guid, data) {
+    const subscribers = await QueueConnections.subscribers(guid);
+    if (subscribers.Items || subscribers.Items.length === 0) throw new Error('No listeners for ' + guid);
 
     let idx = 0;
     let sub = subscribers.Items[idx];
 
-    while (!send_message(sub.connectionId.S, request)) {
+    while (!send_message(apiGwClient, sub.connectionId.S, data)) {
         idx++;
 
         if (idx >= subscribers.Items.length) {
-            throw new Error('No listeners for ' + arch);
+            throw new Error('No listeners for ' + guid);
         }
 
         sub = subscribers.Items[idx];
     }
 }
 
-async function handle_text_message(id, message) {
+async function handle_text_message(apiGwClient, connectionId, message) {
     if (typeof message === 'string' && message.startsWith('subscribe: ')) {
-        await QueueConnections.update(id, message.substring(11));
-    } else if (typeof message === 'object' && message.arch) {
-        await relay_request(message.arch, message);
+        await QueueConnections.update(connectionId, message.substring(11));
+    } else if (typeof message === 'object' && message.guid) {
+        await relay_request(apiGwClient, message.guid, message);
     } else {
         // console.error('Someone said something unknown: ' + message);
     }
--- queue-lambda/readme.md
+++ queue-lambda/readme.md
@@ -8,3 +8,17 @@ How it works:
 - `queue-ondisconnect.js` is triggered when there's a disconnect, the connection is deleted from the DynamoDB table.
 - `queue-sendmessage.js` when someone sends a message to the websocket, other connections can be looked up in the
   DynamoDB table and be sent messages to.
+
+
+## Scenario
+
+Client A is a x86 compilation instance
+Client B is a aarch64 execution instance
+
+* Client A has pushed a request for execution
+* Client A connects to websocket
+* Client A says: "subscribe: 1234abc"
+* Client B connects to websocket
+* Client B says: {"guid": "1234abc", "code": "42"}
+* Client A receives: {"guid": "1234abc", "code": "42"}
+* Client A handles the message
