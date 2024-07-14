2c2d0c19ff0a24850444f599feedd882aa621651
Partouf <partouf@gmail.com>
2024-07-14 19:33:53 +0200

    oops

--- start-support.sh
+++ start-support.sh
@@ -30,7 +30,7 @@ mount_opt() {
     [ -f /opt/.health ] || touch /opt/.health
     mountpoint /opt/.health || mount --bind /efs/.health /opt/.health
 
-    if [[ "${SKIP_SQUASH}" == "1" ]]; then
+    if [[ "${SKIP_SQUASH}" == "0" ]]; then
         # don't be tempted to background this, it just causes everything to wedge
         # during startup (startup time I/O etc goes through the roof).
         ./mount-all-img.sh
