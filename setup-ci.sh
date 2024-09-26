008bdcae1c551c4e325d73e9046c50ea8da34ae6
Partouf <partouf@gmail.com>
2024-09-26 17:06:21 +0200

    install docker

--- setup-ci.sh
+++ setup-ci.sh
@@ -9,6 +9,8 @@ cd "${DIR}"
 
 env EXTRA_NFS_ARGS="" "${DIR}/setup-common.sh" ci
 
+wget -qO- https://get.docker.com/ | sh
+
 ln -s /efs/squash-images /opt/squash-images
 ln -s /efs/compiler-explorer /opt/compiler-explorer
 ln -s /efs/wine-stable /opt/wine-stable
