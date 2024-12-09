62aa0339f7adb8efa38ca16c7bbd30374ac9a9dc
Matt Godbolt <matt@godbolt.org>
2024-12-09 15:18:22 +0000

    Olafur's yellow box

--- bin/lib/installable/installable.py
+++ bin/lib/installable/installable.py
@@ -292,9 +292,10 @@ class Installable:
         self._logger.info("Squashing %s...", source_folder)
         self.install_context.check_call(
             [
-                "mksquashfs",
+                "/usr/bin/mksquashfs",
                 str(source_folder),
                 str(temp_image),
+                "-all-root",
                 "-progress",
                 "-comp",
                 "zstd",
