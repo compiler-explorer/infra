96ae526ae7bf9239fc45217c32aca51ffea202a8
Matt Godbolt <matt@godbolt.org>
2025-01-30 13:14:18 -0600

    Latest aarch64prod

--- terraform/lc.tf
+++ terraform/lc.tf
@@ -3,7 +3,7 @@ locals {
   staging_image_id         = "ami-087bff241f96d60c4"
   beta_image_id            = "ami-087bff241f96d60c4"
   gpu_image_id             = "ami-0e49c31db87fb4332"
-  aarch64prod_image_id     = "ami-0940f416984ac4e8b"
+  aarch64prod_image_id     = "ami-0e26fa5ba17ae338e"
   aarch64staging_image_id  = "ami-0e26fa5ba17ae338e"
   winprod_image_id         = "ami-008a0c51822c0e27f"
   winstaging_image_id      = "ami-008a0c51822c0e27f"
