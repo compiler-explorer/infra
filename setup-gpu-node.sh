b24767d3483241f084e8f3003e1aec1f5c69b48a
Matt Godbolt <matt@godbolt.org>
2025-02-03 10:58:40 -0600

    GPU updates, tests and some docs (#1519)

--- setup-gpu-node.sh
+++ setup-gpu-node.sh
@@ -13,7 +13,40 @@ rm cuda_keyring.deb
 popd
 
 apt-get -y update
-# The CUDA packages include the most appropriate driver for its version.
-# Note the hyphen in the version number, not a period.
+# Note the hyphen in the CUDA version number, not a period.
+DRIVER_VERSION=570
 CUDA_VERSION=12-8
-apt install -y nvidia-headless-570-open nvidia-utils-570 nvidia-driver-assistant cuda-compiler-${CUDA_VERSION} cuda-runtime-${CUDA_VERSION}
+apt install -y \
+    nvidia-headless-${DRIVER_VERSION}-open \
+    nvidia-utils-${DRIVER_VERSION} \
+    nvidia-driver-assistant \
+    cuda-compiler-${CUDA_VERSION} \
+    cuda-runtime-${CUDA_VERSION}
+
+# Ensure the above worked.
+cat <<EOF > /tmp/cuda-test.cu
+#include <cstdio>
+
+int main () {
+  int deviceCount = 0;
+  cudaError_t error_id = cudaGetDeviceCount(&deviceCount);
+
+  if (error_id != cudaSuccess) {
+    printf("cudaGetDeviceCount returned %d\n-> %s\n",
+           static_cast<int>(error_id), cudaGetErrorString(error_id));
+    printf("Result = FAIL\n");
+    exit(EXIT_FAILURE);
+  }
+  if (deviceCount == 0) {
+    printf("There are no available device(s) that support CUDA\n");
+    exit(EXIT_FAILURE);
+  } else {
+    printf("Detected %d CUDA Capable device(s)\n", deviceCount);
+  }
+}
+EOF
+echo Compiling CUDA test...
+/efs/compiler-explorer/cuda/12.6.2/bin/nvcc -o /tmp/cuda-test /tmp/cuda-test.cu
+echo Running CUDA test...
+/tmp/cuda-test
+echo Done
