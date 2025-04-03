040d2026b6ba1d87b9346625bb6b19d859f62d7c
Kevin Jeon <kevinjeon@google.com>
2025-04-03 17:02:53 +0000

    Enclose curly braces in quotes

--- bin/yaml/android-java/fetch_art_release_from_head.sh
+++ bin/yaml/android-java/fetch_art_release_from_head.sh
@@ -8,7 +8,7 @@ set -euo pipefail
 URL=https://ci.android.com/builds/latest/branches/aosp-android-latest-release/targets/aosp_cf_x86_64_only_phone-userdebug/view/BUILD_INFO
 PATTERN="(.*\/submitted\/([0-9]+)\/.*)/view/BUILD_INFO$"
 
-RURL=$(curl -Ls -o /dev/null -w %{url_effective} "${URL}")
+RURL=$(curl -Ls -o /dev/null -w "%{url_effective}" "${URL}")
 if ! [[ "$RURL" =~ $PATTERN ]]; then
   echo "Got expected URL $RURL"
   exit 1
