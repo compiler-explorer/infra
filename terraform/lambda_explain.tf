0b5a6cdf6203cb678909d579650930db11a0721c
Matt Godbolt <matt@godbolt.org>
2025-05-22 13:21:39 -0500

    Bump explainer

--- terraform/lambda_explain.tf
+++ terraform/lambda_explain.tf
@@ -13,7 +13,7 @@ resource "aws_ecr_repository" "explain" {
 
 data "aws_ecr_image" "explain" {
   repository_name = aws_ecr_repository.explain.name
-  image_tag = "gh-11"
+  image_tag = "gh-12"
 }
 
 resource "aws_lambda_function" "explain" {
