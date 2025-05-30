fc6f8d5b765d394bf6c4b4e85863bee61d834c2a
Matt Godbolt <matt@godbolt.org>
2025-05-30 16:41:28 -0500

    Better claude isolation

--- terraform/lambda_explain.tf
+++ terraform/lambda_explain.tf
@@ -33,9 +33,53 @@ resource "aws_lambda_function" "explain" {
       ROOT_PATH = "/explain"
       METRICS_ENABLED = "true"
       CACHE_ENABLED = "true"
-      CACHE_S3_BUCKET = aws_s3_bucket.storage-godbolt-org.bucket
-      CACHE_S3_PREFIX = "explain-cache/"
-      CACHE_S3_TTL = "2d"
+      CACHE_S3_BUCKET = aws_s3_bucket.claude-explain-cache-godbolt-org.bucket
+      CACHE_S3_PREFIX = ""
+      CACHE_S3_TTL = "2d"  # TTL is the client-side TTL
+    }
+  }
+}
+
+// Bucket configuration; set to make this whole thing easy to remove if we decide it's a bad idea.
+resource "aws_s3_bucket" "claude-explain-cache-godbolt-org" {
+  bucket = "claude-explain-cache.godbolt.org"
+  tags = {
+    S3-Bucket-Name = "claude-explain-cache.godbolt.org"
+  }
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "claude-explain-cache-godbolt-org" {
+  bucket = aws_s3_bucket.claude-explain-cache-godbolt-org.id
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "claude-explain-cache-godbolt-org" {
+  bucket = aws_s3_bucket.claude-explain-cache-godbolt-org.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "claude-explain-cache-godbolt-org" {
+  bucket = aws_s3_bucket.claude-explain-cache-godbolt-org.id
+  rule {
+    id     = "Remove cached items"
+    status = "Enabled"
+    expiration {
+      days = 3
+    }
+    noncurrent_version_expiration {
+      noncurrent_days = 1
+    }
+    filter {
+      prefix = ""
     }
   }
 }
@@ -48,13 +92,13 @@ data "aws_iam_policy_document" "explain_s3_access" {
       "s3:GetObject",
       "s3:PutObject"
     ]
-    resources = ["${aws_s3_bucket.storage-godbolt-org.arn}/explain-cache/*"]
+    resources = ["${aws_s3_bucket.claude-explain-cache-godbolt-org.arn}/*"]
   }
 }
 
 resource "aws_iam_policy" "explain_s3_access" {
   name        = "explain_lambda_s3_access"
-  description = "Allow explain lambda to access explain-cache prefix in storage bucket"
+  description = "Allow explain lambda to access explain-cache prefix in cache bucket"
   policy      = data.aws_iam_policy_document.explain_s3_access.json
 }
 
