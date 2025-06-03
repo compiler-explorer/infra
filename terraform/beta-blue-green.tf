c14225ab3e56ff666234e8b0e0a28639192a2b4d
Partouf <partouf@gmail.com>
2025-06-03 15:50:27 +0200

    obsolete things

--- terraform/beta-blue-green.tf
+++ terraform/beta-blue-green.tf
@@ -15,31 +15,3 @@ module "beta_blue_green" {
   enabled_metrics           = local.common_enabled_metrics
   initial_active_color      = "blue"
 }
-
-# Outputs for reference and use in other resources
-output "beta_blue_target_group_arn" {
-  value       = module.beta_blue_green.target_group_arns["blue"]
-  description = "ARN of the beta blue target group"
-}
-
-output "beta_green_target_group_arn" {
-  value       = module.beta_blue_green.target_group_arns["green"]
-  description = "ARN of the beta green target group"
-}
-
-output "beta_active_target_group_arn" {
-  value       = module.beta_blue_green.active_target_group_arn
-  description = "Currently active target group ARN for beta"
-  sensitive   = true
-}
-
-output "beta_active_color" {
-  value       = data.aws_ssm_parameter.beta_active_color.value
-  description = "Currently active color for beta environment"
-  sensitive   = true
-}
-
-# Data source for active color parameter (for compatibility)
-data "aws_ssm_parameter" "beta_active_color" {
-  name = module.beta_blue_green.active_color_parameter_name
-}
