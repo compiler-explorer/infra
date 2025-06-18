d7c29ce7c7951cc71c9d472d8a3aceeab2667d47
Matt Godbolt <matt@godbolt.org>
2025-06-18 13:59:31 -0500

    Bump admin node to handle rust-miri-nightly installs

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -21,7 +21,7 @@ resource "aws_instance" "AdminNode" {
 
   root_block_device {
     volume_type           = "gp2"
-    volume_size           = 24
+    volume_size           = 40
     delete_on_termination = false
   }
 
