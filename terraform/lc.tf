2607e63d514f2db7b09627801fe4b9dcfe095abe
Matt Godbolt <matt@godbolt.org>
2025-08-08 13:43:49 -0500

    New staging image 22.04 but with these changes

--- terraform/lc.tf
+++ terraform/lc.tf
@@ -1,6 +1,6 @@
 locals {
   image_id                 = "ami-01297581addf1e7dd"
-  staging_image_id         = "ami-0e8dc79a8b7ecccaa"
+  staging_image_id         = "ami-0358f45820da69d16"
   beta_image_id            = "ami-01297581addf1e7dd"
   gpu_image_id             = "ami-09e85330c7a5c8afb"
   aarch64prod_image_id     = "ami-079d9b1e36db465bb"
