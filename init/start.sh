a32bddc67b4f075ab35f6dffe63bfa8031fe2d0c
Partouf <partouf@gmail.com>
2025-08-16 21:25:36 +0200

    fail if simple execution with nsjail fails on startup

--- init/start.sh
+++ init/start.sh
@@ -13,6 +13,9 @@ LOG_DEST_PORT=$(get_conf /compiler-explorer/logDestPort)
 setup_cgroups
 mount_opt
 mount_nosym
+
+sudo -u ce nsjail --config /infra/.deploy/etc/nsjail/compilers-and-tools.cfg -- /bin/bash -c "echo working"
+
 update_code
 
 COMPILERS_ARG=
