5d8c8b10918c30db64b98114c153df90a7f5bccd
Matt Godbolt <matt@godbolt.org>
2025-08-18 08:42:19 -0500

    Add a bit more debugging

--- init/start.sh
+++ init/start.sh
@@ -15,8 +15,9 @@ mount_opt
 mount_nosym
 update_code
 
-if ! sudo -u ${CE_USER} nsjail --config /infra/.deploy/etc/nsjail/compilers-and-tools.cfg -- /bin/bash -c "echo nsjail works"; then
+if ! sudo -u "${CE_USER}" nsjail --config /infra/.deploy/etc/nsjail/compilers-and-tools.cfg -- /bin/bash -c "echo nsjail works"; then
     echo "nsjail is not working, please check the configuration."
+    log_cgroups
     exit 1
 fi
 
