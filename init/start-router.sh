1a28eee91bf710ed0e25d0c5001bf3db6e572f58
Partouf <partouf@gmail.com>
2025-09-11 22:59:51 +0200

    not needed

--- init/start-router.sh
+++ init/start-router.sh
@@ -11,17 +11,9 @@ LOG_DEST_PORT=$(get_conf /compiler-explorer/logDestPortRouter)
 install_ce_router
 cd /infra/.deploy
 
-# Get SQS overflow configuration from SSM parameters
-SQS_MAX_MESSAGE_SIZE=$(get_conf /compiler-explorer/sqsMaxMessageSize)
-S3_OVERFLOW_BUCKET=$(get_conf /compiler-explorer/s3OverflowBucket)
-S3_OVERFLOW_KEY_PREFIX=$(get_conf /compiler-explorer/s3OverflowKeyPrefix)
-
 # shellcheck disable=SC2086
 exec sudo -u ${CE_USER} -H --preserve-env=NODE_ENV -- \
     env NODE_ENV=production \
-    SQS_MAX_MESSAGE_SIZE="${SQS_MAX_MESSAGE_SIZE}" \
-    S3_OVERFLOW_BUCKET="${S3_OVERFLOW_BUCKET}" \
-    S3_OVERFLOW_KEY_PREFIX="${S3_OVERFLOW_KEY_PREFIX}" \
     /opt/node/bin/node \
     -- index.js \
     --env "${ENV}" \
