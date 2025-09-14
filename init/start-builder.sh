8185939cc3f4b6dd9673ef7a33f4be833c7f1dca
Partouf <partouf@gmail.com>
2025-09-14 16:07:44 +0200

    init lib builder via workflow

---
+++ init/start-builder.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -ex
+
+export CONAN_PASSWORD = $1;
+LANGUAGE=$2
+LIBRARYTOBUILD=$3
+FORCECOMPILER=$4
+
+FORCECOMPILERPARAM=""
+if [ "$FORCECOMPILER" = "popular-compilers-only" ]; then
+  FORCECOMPILERPARAM="--popular-compilers-only"
+elif [ "$FORCECOMPILER" != "all" ]; then
+  FORCECOMPILERPARAM="--buildfor=$FORCECOMPILER"
+fi
+
+LIBRARYPARAM="libraries/$LANGUAGE"
+if [ "$LIBRARYTOBUILD" != "all" ]; then
+  LIBRARYPARAM="libraries/$LANGUAGE/$LIBRARYTOBUILD"
+fi
+
+PATH=$PATH:/opt/compiler-explorer/cmake/bin
+
+cd /tmp/build
+git clone https://github.com/compiler-explorer/infra
+
+cd /tmp/build/infra
+
+
+export CONAN_USER = "ce";
+export CONAN_HOME=$(conan config home)
+
+cp /tmp/build/infra/init/settings.yml $CONAN_HOME/settings.yml
+make ce > ceinstall.log
+
+conan user ce -p -r=ceserver
+bin/ce_install --staging-dir=/tmp/staging --enable=nightly build "$FORCECOMPILERPARAM" "$LIBRARYPARAM"
