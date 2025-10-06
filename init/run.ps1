4cbcc1d9c0c71e3b291baceec5625d3423a3c680
Partouf <partouf@gmail.com>
2025-10-06 23:09:59 +0200

    comment out new code

--- init/run.ps1
+++ init/run.ps1
@@ -8,51 +8,67 @@ param(
     $InstanceColor = $null
 )
 
-function MountY {
-    $exists = (Get-SmbMapping -LocalPath 'Y:') -as [bool]
+function MountZ {
+    $exists = (Get-SmbMapping -LocalPath 'Z:') -as [bool]
     if ($exists) {
-         Remove-SmbMapping -LocalPath 'Y:' -Force
+         Remove-SmbMapping -LocalPath 'Z:' -Force
          $exists = $False
     }
 
     while (-not $exists) {
         try {
-            Write-Host "Mapping Y:"
-            $exists = (New-SmbMapping -LocalPath 'Y:' -RemotePath "\\$SMBServer\winshared") -as [bool]
+            Write-Host "Mapping Z:"
+            $exists = (New-SmbMapping -LocalPath 'Z:' -RemotePath "\\$SMBServer\winshared") -as [bool]
         } catch {
         }
     }
 }
 
-function Wait-ForDrive {
-    param(
-        [Parameter(Mandatory=$true)]
-        [string]$DriveLetter,
-        [int]$CheckIntervalSeconds = 1
-    )
+# function MountY {
+#     $exists = (Get-SmbMapping -LocalPath 'Y:') -as [bool]
+#     if ($exists) {
+#          Remove-SmbMapping -LocalPath 'Y:' -Force
+#          $exists = $False
+#     }
 
-    Write-Host "Waiting for drive $DriveLetter`:\" -ForegroundColor Yellow
+#     while (-not $exists) {
+#         try {
+#             Write-Host "Mapping Y:"
+#             $exists = (New-SmbMapping -LocalPath 'Y:' -RemotePath "\\$SMBServer\winshared") -as [bool]
+#         } catch {
+#         }
+#     }
+# }
 
-    while ($true) {
-        Start-Sleep -Seconds $CheckIntervalSeconds
+# function Wait-ForDrive {
+#     param(
+#         [Parameter(Mandatory=$true)]
+#         [string]$DriveLetter,
+#         [int]$CheckIntervalSeconds = 1
+#     )
 
-        # Check if the specific drive is available
-        $currentDrives = Get-PSDrive -PSProvider FileSystem | Where-Object { $_.Name.Length -eq 1 } | Select-Object -ExpandProperty Name
+#     Write-Host "Waiting for drive $DriveLetter`:\" -ForegroundColor Yellow
 
-        if ($currentDrives -contains $DriveLetter) {
-            Write-Host "`nDrive $DriveLetter`:\ is now available!" -ForegroundColor Green
-            return $true
-        }
+#     while ($true) {
+#         Start-Sleep -Seconds $CheckIntervalSeconds
 
-        Write-Host "." -NoNewline -ForegroundColor DarkGray
-    }
-}
+#         # Check if the specific drive is available
+#         $currentDrives = Get-PSDrive -PSProvider FileSystem | Where-Object { $_.Name.Length -eq 1 } | Select-Object -ExpandProperty Name
 
-MountY
+#         if ($currentDrives -contains $DriveLetter) {
+#             Write-Host "`nDrive $DriveLetter`:\ is now available!" -ForegroundColor Green
+#             return $true
+#         }
 
-Start-Process "C:\tmp\cewinfilecache\CeWinFileCacheFS.exe" -WorkingDirectory "C:\tmp\cewinfilecache" -ArgumentList "--mount Z: --log-level info --config compilers.production.json" -NoNewWindow
+#         Write-Host "." -NoNewline -ForegroundColor DarkGray
+#     }
+# }
 
-Wait-ForDrive -DriveLetter 'Z' -CheckIntervalSeconds 1
+# MountY
+
+# Start-Process "C:\tmp\cewinfilecache\CeWinFileCacheFS.exe" -WorkingDirectory "C:\tmp\cewinfilecache" -ArgumentList "--mount Z: --log-level info --config compilers.production.json" -NoNewWindow
+
+# Wait-ForDrive -DriveLetter 'Z' -CheckIntervalSeconds 1
 
 $env:NODE_ENV = "production"
 $env:PATH = "$env:PATH;Z:/compilers/mingw-8.1.0/mingw64/bin"
