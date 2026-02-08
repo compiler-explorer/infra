7c49ac4564ee708c833b01ad68f59aaf3ac2d2c6
Matt Godbolt (bot acct) <mattgodbolt+molty@godbolt.org>
2026-02-08 11:44:15 -0600

    feat(msvc): add MSVC 14.50 (VS 2026 18.0.0 and 18.2.1) (#1962)
    Co-authored-by: mattgodbolt-molty <mattgodbolt-molty@users.noreply.github.com>

--- msvc-install/gen-props.py
+++ msvc-install/gen-props.py
@@ -37,6 +37,8 @@ minimum_install_req = [
   # {"MSVersionSemver": "14.43.34433", "MSVSVer": "2022", "MSVSShortVer": "17.12.13", "ZIPFile": "14.42.34433-14.42.34444.0"},
     {"MSVersionSemver": "14.43.34808", "MSVSVer": "2022", "MSVSShortVer": "17.13.6", "ZIPFile": "14.43.34808-14.43.34810.0"},
     {"MSVersionSemver": "14.44.35207", "MSVSVer": "2022", "MSVSShortVer": "17.14.19", "ZIPFile": "14.44.35207-14.44.35219.0"},
+    {"MSVersionSemver": "14.50.35717", "MSVSVer": "2026", "MSVSShortVer": "18.0.0", "ZIPFile": "14.50.35717-14.50.35717.0"},
+    {"MSVersionSemver": "14.50.35723", "MSVSVer": "2026", "MSVSShortVer": "18.2.1", "ZIPFile": "14.50.35717-14.50.35723.0"},
 ]
 # fmt: on
 
